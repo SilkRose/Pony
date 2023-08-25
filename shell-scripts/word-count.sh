930f771d2494097f3899d54dba179821c0b7d002
Silk Rose <silkrose@love-tolerance.com>
2023-08-25 00:02:32 -0400

    Update word count, fix flash fiction link counting

--- shell-scripts/word-count.sh
+++ shell-scripts/word-count.sh
@@ -3,7 +3,13 @@
 md_files=$(find "../" -type f -name "*.md" -not -name "*-meta.md" -not -name "ideas.md" -not -name "names.md" -not -name "README.md")
 
 for file in $md_files; do
-    word_count=$(tr -d "#>\-*–|—" < "$file" | sed '/<!--.*?-->/d' | wc -w)
+    word_count=$(tr -d "#>\-*–|—" < "$file" | sed '/<!--.*-->/d' | wc -w)
+    case "$file" in
+       *"/flash-fiction/"*)
+          link_word_count=$(grep -E '\[.*\]\(.*\)' "$file" | wc -w)
+          word_count=$((word_count - link_word_count))
+          ;;
+    esac
     total_word_count=$((total_word_count + word_count))
 done
 
