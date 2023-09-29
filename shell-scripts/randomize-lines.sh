8093c7bb0268e170da131058b328a7a0dd72a6b4
Silk Rose <silkrose@love-tolerance.com>
2023-09-28 20:55:31 -0400

    Add randomize lines shell script

---
+++ shell-scripts/randomize-lines.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+file="$1"
+
+line_one=$(head -n 1 "$file")
+lines=$(sed '1d;/^$/d' "$file")
+shuffled_lines=$(echo "$lines" | shuf)
+shuffled_lines_with_empty_lines=$(echo "$shuffled_lines" | sed G)
+result="$line_one\n\n$shuffled_lines_with_empty_lines"
+
+echo "$result" > "$file"
