dff4ab1367786b670bbcfa7d1e0f496d919304a3
Silk Rose <silkrose@love-tolerance.com>
2023-09-28 20:33:26 -0400

    Remove archive from word count script results

--- shell-scripts/word-count.sh
+++ shell-scripts/word-count.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-md_files=$(find "../" -type f -name "*.md" -not -name "*-meta.md" -not -name "ideas.md" -not -name "names.md" -not -name "README.md")
+md_files=$(find "../" -type f -name "*.md" \
+    -not -name "*-meta.md" \
+    -not -name "ideas.md" \
+    -not -name "names.md" \
+    -not -name "README.md" \
+    -not -path "*/archive/*")
 
 for file in $md_files; do
     word_count=$(tr -d "#>\-*–|—" < "$file" | sed '/<!--.*-->/d' | wc -w)
