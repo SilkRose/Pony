5f423c830d9fce84cca10de89bf4fa717b55fc45
Silk Rose <silkrose@love-tolerance.com>
2023-10-11 07:47:42 -0400

    Rewrite static api script

--- shell-scripts/static-api.sh
+++ shell-scripts/static-api.sh
@@ -6,20 +6,36 @@ fi
 
 mkdir "./dist"
 
-covers=$(sh "./cover-count.sh" | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
-flash_fictions=$(sh "./flash-fiction-count.sh" | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
-ideas=$(sh "./idea-count.sh" | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
-names=$(sh "./name-count.sh" | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
-stories=$(sh "./story-count.sh" | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
-words=$(sh "./word-count.sh" | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
+count_and_format() {
+	script="$1"
+	directory="$2"
+	count=$(sh "$script" "$(./location-finder.sh "$directory")")
+	echo "$count" | sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta'
+}
 
-json=$(cat "api-template.json")
+covers=$(count_and_format "./cover-count.sh" "stories")
+flash_fiction=$(count_and_format "./flash-fiction-count.sh" "flash-fiction")
+ideas=$(count_and_format "./idea-count.sh" "ideas")
+names=$(count_and_format "./name-count.sh" "names")
+stories=$(count_and_format "./story-count.sh" "stories")
+words=$(sh "./word-count.sh" | sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta')
 
-json=$(echo "$json" | sed -e "s/\$covers/$covers/g" \
-	-e "s/\$flash-fictions/$flash_fictions/g" \
-	-e "s/\$ideas/$ideas/g" \
-	-e "s/\$names/$names/g" \
-	-e "s/\$stories/$stories/g" \
-	-e "s/\$words/$words/g")
+json=$(jq --null-input --tab \
+	--arg covers "$covers" \
+	--arg flash_fiction "$flash_fiction" \
+	--arg ideas "$ideas" \
+	--arg names "$names" \
+	--arg stories "$stories" \
+	--arg words "$words" \
+	'{
+		"covers": $covers,
+		"flash_fiction": $flash_fiction,
+		"ideas": $ideas,
+		"names": $names,
+		"stories": $stories,
+		"words": $words
+	} | to_entries
+	| map(.key |= gsub("_"; "-"))
+	| from_entries')
 
 echo "$json" > "./dist/pony.json"
