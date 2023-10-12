273fe52f9d8d9c12c7b505de3a4dea645b21159f
Silk Rose <silkrose@love-tolerance.com>
2023-10-12 09:26:07 -0400

    Update word count shell script
    Rewrite to be more robust and accurate.

--- shell-scripts/word-count.sh
+++ shell-scripts/word-count.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env sh
 
-md_files=$(find "../" -type f -name "*.md" \
+stories_location=$1
+flash_fiction_location=$2
+
+md_files=$(find "$stories_location" -type f -name "*.md" \
 	-not -name "*-meta.md" \
 	-not -name "ideas.md" \
 	-not -name "names.md" \
-	-not -name "README.md" \
-	-not -path "*/archive/*")
+	-not -wholename "*darling-and-date/meta.md")
 
 for file in $md_files; do
-	word_count=$(tr -d "#>\-*–|—" < "$file" | sed '/<!--.*-->/d' | wc -w)
-	case "$file" in
-		*"/flash-fiction/"*)
-			link_word_count=$(grep -E '\[.*\]\(.*\)' "$file" | wc -w)
-			word_count=$((word_count - link_word_count))
-			;;
-	esac
+	word_count=$(sed '/<center>\*\*\*<\/center>\|<p align="center">\*\*\*<\/p>/d' < "$file" \
+		| tr -d "#>\-*–|—" | sed '/<!--.*-->/d' | wc -w)
 	total_word_count=$((total_word_count + word_count))
 done
+
+if [ -n "$flash_fiction_location" ]; then
+	md_files=$(find "$flash_fiction_location" -type f -name "*.md")
+	for file in $md_files; do
+		word_count=$(sed '/<center>\*\*\*<\/center>\|<p align="center">\*\*\*<\/p>/d' < "$file" \
+			| tr -d "#>\-*–|—" | sed '/<!--.*-->/d' | wc -w)
+		link_word_count=$(grep -E '\[.*\]\(.*\)' "$file" | wc -w)
+		word_count=$((word_count - link_word_count))
+		total_word_count=$((total_word_count + word_count))
+	done
+fi
 
 if [ -n "$total_word_count" ]; then
 	echo "$total_word_count"
