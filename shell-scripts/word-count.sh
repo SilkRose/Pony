2c6a2f40222be355aa8cccd265a8c19df7143f46
Silk Rose <silkrose@love-tolerance.com>
2023-10-12 10:06:52 -0400

    Update word count script to check for variable

--- shell-scripts/word-count.sh
+++ shell-scripts/word-count.sh
@@ -3,17 +3,18 @@
 stories_location=$1
 flash_fiction_location=$2
 
-md_files=$(find "$stories_location" -type f -name "*.md" \
-	-not -name "*-meta.md" \
-	-not -name "ideas.md" \
-	-not -name "names.md" \
-	-not -wholename "*darling-and-date/meta.md")
-
-for file in $md_files; do
-	word_count=$(sed '/<center>\*\*\*<\/center>\|<p align="center">\*\*\*<\/p>/d' < "$file" \
-		| tr -d "#>\-*–|—" | sed '/<!--.*-->/d' | wc -w)
-	total_word_count=$((total_word_count + word_count))
-done
+if [ -n "$stories_location" ]; then
+	md_files=$(find "$stories_location" -type f -name "*.md" \
+		-not -name "*-meta.md" \
+		-not -name "ideas.md" \
+		-not -name "names.md" \
+		-not -wholename "*darling-and-date/meta.md")
+	for file in $md_files; do
+		word_count=$(sed '/<center>\*\*\*<\/center>\|<p align="center">\*\*\*<\/p>/d' < "$file" \
+			| tr -d "#>\-*–|—" | sed '/<!--.*-->/d' | wc -w)
+		total_word_count=$((total_word_count + word_count))
+	done
+fi
 
 if [ -n "$flash_fiction_location" ]; then
 	md_files=$(find "$flash_fiction_location" -type f -name "*.md")
