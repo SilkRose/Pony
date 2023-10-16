bc344017ffc107434d21b1705cd8c55d62226b79
Silk Rose <silkrose@love-tolerance.com>
2023-10-16 16:13:10 -0400

    Limit cover count to one per folder

--- shell-scripts/cover-count.sh
+++ shell-scripts/cover-count.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env sh
 
-location=$1
+location="$1"
 
 if [ -n "$location" ]; then
-	cover_count=$(find "$location" -type f -name "*cover*.*" \
-		-not -name "*concept*" \
-		-not -name "*.xcf" \
-		-not -name "*upscaled*.*" | wc -l)
+	cover_count=0
+	story_folders=$(find "$location" -mindepth 1 -maxdepth 1 -type d)
+	for folder in $story_folders; do
+		cover_folder_count=$(find "$folder" -type f -name "*cover*.*" \
+			-not -name "*concept*" \
+			-not -name "*.xcf" \
+			-not -name "*upscaled*.*" | wc -l)
 
+		if [ "$cover_folder_count" -gt 0 ]; then
+			cover_count=$((cover_count + 1))
+		fi
+	done
 	echo "$cover_count"
 else
 	echo 0
