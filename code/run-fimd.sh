8bcc0c727be3446783cc8fb9a79c99128f530d69
Silk Rose <silkrose@love-tolerance.com>
2023-11-12 06:04:14 -0500

    Update run-fimd.sh

--- code/run-fimd.sh
+++ code/run-fimd.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env sh
 
+set -o errexit
+set -o nounset
+
 if ! command -v fimd > /dev/null 2>&1; then
 	echo "fimd is not installed. Please install it."
 	exit 1
 fi
 
-if [ -d "./publish" ]; then
-	rm -rf "./publish"
+if [ -d ./publish ]; then
+	rm -rf ./publish
 fi
 
-mkdir "./publish"
+mkdir ./publish
 
 md_files=$(find "../" -type f -name "*.md" \
 	-not -name "ideas.md" \
