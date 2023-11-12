bd751ae03841e6f747b4a124c748406a01514189
Silk Rose <silkrose@love-tolerance.com>
2023-11-12 05:56:26 -0500

    Update json-format.sh

--- code/json-format.sh
+++ code/json-format.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
-input=$1
+set -o errexit
+set -o nounset
 
-if [ -n "$input" ]; then
-	jq --tab . "$input" > "$input.tmp" && mv "$input.tmp" "$input"
-else
-	find . -type f -name "*.json" | while IFS= read -r file; do
-		jq --tab . "$file" > "$file.tmp" && mv "$file.tmp" "$file"
-	done
+if ! command -v jq > /dev/null 2>&1; then
+	echo "jq is not installed. Please install it."
+	exit 1
 fi
+
+find . -type f -name "*.json" | while IFS= read -r file; do
+	jq --tab . "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+done
