75df3582d9750f6703e5e3c7af24afae73eaabbc
Silk Rose <silkrose@love-tolerance.com>
2023-11-14 04:06:55 -0500

    Add commit data function, commit and stats types

--- code/api.ts
+++ code/api.ts
@@ -7,6 +7,22 @@ import * as pfs from "./lib/pfs.ts";
 
 await mane();
 
+type Commit = {
+	hash: string;
+	subject: string;
+	unix_time: number;
+	stats: Stats;
+};
+
+type Stats = {
+	covers: string;
+	flash_fiction: string;
+	ideas: string;
+	names: string;
+	stories: string;
+	words: string;
+};
+
 const links = {
 	pony: "https://pony.silkrose.dev/api/v1/pony.json",
 	pony_commits: "https://pony.silkrose.dev/api/v1/pony-commits.json",
@@ -22,5 +38,30 @@ async function mane() {
 	const git_url = repository.url.slice(4);
 	pexec.executeCommand(`git clone --quiet ${git_url} pony-temp`);
 	process.chdir("./pony-temp");
-	console.log(process.cwd());
+	const git_log = pexec.executeCommandReturn(
+		'git log mane --format="format:%H\n%s\n%ct"'
+	);
+	const commits = getCommitData(git_log);
+	console.log(commits);
+}
+
+function getCommitData(git_log: string) {
+	let commits: Commit[] = [];
+	const log = git_log.split("\n");
+	for (let i = 0; i < log.length; i += 3) {
+		commits.push({
+			hash: log[i],
+			subject: log[i + 1],
+			unix_time: Number(log[i + 2]),
+			stats: {
+				covers: "",
+				flash_fiction: "",
+				ideas: "",
+				names: "",
+				stories: "",
+				words: "",
+			},
+		});
+	}
+	return commits;
 }
