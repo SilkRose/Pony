3c066e74406f135bea167d3910f4db4c658aef35
Silk Rose <silkrose@love-tolerance.com>
2023-11-14 01:43:42 -0500

    Add links var, git cloning, changing directory

--- code/api.ts
+++ code/api.ts
@@ -1,14 +1,25 @@
 #!/usr/bin/env bun
 
 import "@total-typescript/ts-reset";
-import { rmDir, mkDirs, checkInstalled, writeFile } from "./lib.ts";
+import { repository } from "./package.json";
+import * as lib from "./lib.ts";
 
 await mane();
 
+const links = {
+	pony: "https://pony.silkrose.dev/api/v1/pony.json",
+	pony_commits: "https://pony.silkrose.dev/api/v1/pony-commits.json",
+	hashes: "https://pony.silkrose.dev/shell-script-hashes",
+};
+
 async function mane() {
-	checkInstalled(["git"]);
-	rmDir("./dist");
-	mkDirs(["./dist/api/v1"]);
-	writeFile("./dist/.nojekyll", "");
-	writeFile("./dist/CNAME", "pony.silkrose.dev");
+	lib.checkInstalled(["git"]);
+	lib.rmDirs(["./dist", "./pony-temp"]);
+	lib.mkDirs(["./dist/api/v1"]);
+	lib.writeFile("./dist/.nojekyll", "");
+	lib.writeFile("./dist/CNAME", "pony.silkrose.dev");
+	const git_url = repository.url.slice(4);
+	lib.executeCommand(`git clone --quiet ${git_url} pony-temp`);
+	process.chdir("./pony-temp");
+	console.log(process.cwd());
 }
