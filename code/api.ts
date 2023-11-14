4f7e953d7e1389d19a8ffcc0b47989f363beb9d5
Silk Rose <silkrose@love-tolerance.com>
2023-11-14 00:24:25 -0500

    Start of API Typescript rewrite

--- code/api.ts
+++ code/api.ts
@@ -1,5 +1,14 @@
 #!/usr/bin/env bun
 
 import "@total-typescript/ts-reset";
+import { rmDir, mkDirs, checkInstalled, writeFile } from "./lib.ts";
 
-console.log("Hello world!");
+await mane();
+
+async function mane() {
+	checkInstalled(["git"]);
+	rmDir("./dist");
+	mkDirs(["./dist/api/v1"]);
+	writeFile("./dist/.nojekyll", "");
+	writeFile("./dist/CNAME", "pony.silkrose.dev");
+}
