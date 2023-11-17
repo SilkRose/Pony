528bb27a4707ac7d83038376581c08a105b02927
Silk Rose <silkrose@love-tolerance.com>
2023-11-17 10:19:43 -0500

    Add python graph script
    Script mostly written by ChatGPT.

---
+++ code/graph.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import json
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from datetime import datetime
+
+def format_y_ticks(value, _):
+    if value >= 1_000_000:
+        return f"{value/1e6:.0f}M"
+    elif value >= 1000:
+        return f"{value/1000:.0f}K"
+    else:
+        return str(int(value))
+
+with open("dist/api/v1/pony-commits.json", "r") as file:
+    data = json.load(file)
+
+times = [datetime.utcfromtimestamp(entry["unix_time"]) for entry in data][::-1]
+word_counts = [entry["words"] for entry in data][::-1]
+
+plt.style.use("dark_background")
+
+plt.plot(times, word_counts, color="#CC9CDF")
+
+plt.title("Word Count Over Time")
+plt.xlabel("Time")
+plt.ylabel("Word Count")
+
+start_date = min(times)
+end_date = max(times)
+date_range = end_date - start_date
+num_ticks = 6
+date_interval = date_range / (num_ticks - 1)
+
+tick_dates = [start_date + i * date_interval for i in range(num_ticks)]
+tick_labels = [date.strftime("%Y-%m-%d") for date in tick_dates]
+
+plt.xticks(tick_dates, tick_labels, rotation=35)
+
+plt.gca().yaxis.set_major_formatter(FuncFormatter(format_y_ticks))
+
+plt.tight_layout()
+plt.show()
