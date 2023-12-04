0d8c2093e3ccfd212a9caec91e1fd70cef65460d
Silk Rose <silkrose@love-tolerance.com>
2023-12-04 09:41:55 -0500

    Add file finding to clean.rs

--- code/src/bin/clean.rs
+++ code/src/bin/clean.rs
@@ -1,8 +1,16 @@
-use pdt::pdtfs::find_files_in_dir;
+use pony::fs::find_files_in_dir;
+use regex::Regex;
 
 fn main() {
-	let extensions = Some(vec![".md".to_string()]);
-	let files = find_files_in_dir("../", true, &extensions);
+	let includes = Some(vec![
+		Regex::new(r"stories|flash-fiction").unwrap(),
+		Regex::new(r".md$").unwrap(),
+	]);
+	let excludes = Some(vec![
+		Regex::new(r"archive").unwrap(),
+		Regex::new(r"README.md$").unwrap(),
+	]);
+	let files = find_files_in_dir("../", true, &includes, &excludes);
 	for file in files.iter() {
 		println!("{file}")
 	}
