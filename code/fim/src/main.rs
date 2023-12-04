0fcac874a0088dc1309ebb13c0a87701937a6be4
Silk Rose <silkrose@love-tolerance.com>
2023-12-04 08:42:53 -0500

    Add file check to fim

--- code/fim/src/main.rs
+++ code/fim/src/main.rs
@@ -1,3 +1,9 @@
+use camino::Utf8Path;
+use std::env;
+
 fn main() {
-	todo!();
+	let filename = &env::args().collect::<Vec<_>>()[1];
+	let path = Utf8Path::exists(Utf8Path::new(filename));
+	println!("{:?}", env::current_dir());
+	println!("{path:?}");
 }
