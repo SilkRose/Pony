6b6b9d7d846bd5bed4bccb2ee45e3e14ef86c29f
Silk Rose <silkrose@love-tolerance.com>
2023-12-06 16:09:43 -0500

    Start to fimdoc parser

--- code/fimdoc/src/main.rs
+++ code/fimdoc/src/main.rs
@@ -1,3 +1,35 @@
+use camino::Utf8Path;
+use markdown::tokenize;
+use std::env;
+use std::fs;
+
 fn main() {
-	println!("Hello, world!");
+	let filename = &env::args().collect::<Vec<_>>()[1];
+	if !filename.ends_with(".md") {
+		panic!("File must be Markdown.")
+	};
+	let filepath = Utf8Path::new(filename);
+	if Utf8Path::exists(filepath) {
+		let md = fs::read_to_string(filepath).unwrap();
+		let tokens = tokenize(&md);
+		println!("{md}");
+		tokens.iter().for_each(|t| match t {
+			markdown::Block::Header(text, hl) => {
+				println!("{text:?}, {hl}")
+			}
+			markdown::Block::Paragraph(text) => {
+				println!("{text:?}")
+			}
+			markdown::Block::Blockquote(_) => todo!(),
+			markdown::Block::CodeBlock(_, _) => todo!(),
+			markdown::Block::OrderedList(_, _) => todo!(),
+			markdown::Block::UnorderedList(_) => todo!(),
+			markdown::Block::Raw(text) => {
+				println!("{text}")
+			}
+			markdown::Block::Hr => todo!(),
+		})
+	} else {
+		eprintln!("File not found!");
+	}
 }
