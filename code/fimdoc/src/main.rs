5abae18bd55cce16182f8d166429881e042f1495
Silk Rose <silkrose@love-tolerance.com>
2023-12-06 17:07:00 -0500

    Update markdown dependency version for fimdoc

--- code/fimdoc/src/main.rs
+++ code/fimdoc/src/main.rs
@@ -1,5 +1,5 @@
 use camino::Utf8Path;
-use markdown::tokenize;
+use markdown::{to_mdast, ParseOptions};
 use std::env;
 use std::fs;
 
@@ -11,23 +11,49 @@ fn main() {
 	let filepath = Utf8Path::new(filename);
 	if Utf8Path::exists(filepath) {
 		let md = fs::read_to_string(filepath).unwrap();
-		let tokens = tokenize(&md);
+		let options = ParseOptions::default();
+		let tokens = to_mdast(&md, &options).unwrap();
 		println!("{md}");
-		tokens.iter().for_each(|t| match t {
-			markdown::Block::Header(text, hl) => {
-				println!("{text:?}, {hl}")
+		println!("{tokens:?}");
+		tokens.children().unwrap().iter().for_each(|t| match t {
+			markdown::mdast::Node::Root(_) => todo!(),
+			markdown::mdast::Node::BlockQuote(_) => todo!(),
+			markdown::mdast::Node::FootnoteDefinition(_) => todo!(),
+			markdown::mdast::Node::MdxJsxFlowElement(_) => todo!(),
+			markdown::mdast::Node::List(_) => todo!(),
+			markdown::mdast::Node::MdxjsEsm(_) => todo!(),
+			markdown::mdast::Node::Toml(_) => todo!(),
+			markdown::mdast::Node::Yaml(_) => todo!(),
+			markdown::mdast::Node::Break(_) => todo!(),
+			markdown::mdast::Node::InlineCode(_) => todo!(),
+			markdown::mdast::Node::InlineMath(_) => todo!(),
+			markdown::mdast::Node::Delete(_) => todo!(),
+			markdown::mdast::Node::Emphasis(_) => todo!(),
+			markdown::mdast::Node::MdxTextExpression(_) => todo!(),
+			markdown::mdast::Node::FootnoteReference(_) => todo!(),
+			markdown::mdast::Node::Html(_) => todo!(),
+			markdown::mdast::Node::Image(_) => todo!(),
+			markdown::mdast::Node::ImageReference(_) => todo!(),
+			markdown::mdast::Node::MdxJsxTextElement(_) => todo!(),
+			markdown::mdast::Node::Link(_) => todo!(),
+			markdown::mdast::Node::LinkReference(_) => todo!(),
+			markdown::mdast::Node::Strong(_) => todo!(),
+			markdown::mdast::Node::Text(_) => todo!(),
+			markdown::mdast::Node::Code(_) => todo!(),
+			markdown::mdast::Node::Math(_) => todo!(),
+			markdown::mdast::Node::MdxFlowExpression(_) => todo!(),
+			markdown::mdast::Node::Heading(t) => {
+				println!("{:?}, {:?}", t.children[0], t.depth);
+			},
+			markdown::mdast::Node::Table(_) => todo!(),
+			markdown::mdast::Node::ThematicBreak(_) => todo!(),
+			markdown::mdast::Node::TableRow(_) => todo!(),
+			markdown::mdast::Node::TableCell(_) => todo!(),
+			markdown::mdast::Node::ListItem(_) => todo!(),
+			markdown::mdast::Node::Definition(_) => todo!(),
+			markdown::mdast::Node::Paragraph(t) => {
+				println!("{t:?}");
 			}
-			markdown::Block::Paragraph(text) => {
-				println!("{text:?}")
-			}
-			markdown::Block::Blockquote(_) => todo!(),
-			markdown::Block::CodeBlock(_, _) => todo!(),
-			markdown::Block::OrderedList(_, _) => todo!(),
-			markdown::Block::UnorderedList(_) => todo!(),
-			markdown::Block::Raw(text) => {
-				println!("{text}")
-			}
-			markdown::Block::Hr => todo!(),
 		})
 	} else {
 		eprintln!("File not found!");
