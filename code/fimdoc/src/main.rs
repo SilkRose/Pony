20dfba8c8c909491ab901e43151e5f2573c850c1
Silk Rose <silkrose@love-tolerance.com>
2023-12-08 11:23:06 -0500

    FimDoc rename warn function, add silent to enum

--- code/fimdoc/src/main.rs
+++ code/fimdoc/src/main.rs
@@ -1,12 +1,13 @@
 use camino::Utf8Path;
-use markdown::mdast::{BlockQuote, Heading, Node, Paragraph, Root, ThematicBreak};
+use markdown::mdast::{BlockQuote, Heading, Node, Paragraph, Root};
 use markdown::{to_mdast, ParseOptions};
 use std::env;
 use std::fs;
 
-enum ErrorType {
+enum WarningType {
+	_Fail,
+	_Silent,
 	Warn,
-	Fail,
 }
 
 fn main() {
@@ -21,15 +22,15 @@ fn main() {
 		let tokens = to_mdast(&md, &options).unwrap();
 		println!("{md}");
 		println!("{tokens:?}");
-		const WARN: ErrorType = ErrorType::Warn;
-		let text = handle_node(&tokens, WARN);
+		let warn = WarningType::Warn;
+		let text = handle_node(&tokens, warn);
 		println!("{text}");
 	} else {
 		eprintln!("File not found!");
 	}
 }
 
-fn handle_node(node: &Node, warn: ErrorType) -> String {
+fn handle_node(node: &Node, warn: WarningType) -> String {
 	node.children()
 		.unwrap()
 		.iter()
@@ -38,58 +39,59 @@ fn handle_node(node: &Node, warn: ErrorType) -> String {
 		.join("\n")
 }
 
-fn md_to_bbcode(node: &Node, warn: &ErrorType) -> Option<String> {
+fn md_to_bbcode(node: &Node, warn: &WarningType) -> Option<String> {
 	match node {
 		Node::Root(root) => Some(handle_root(root, warn)),
 		Node::BlockQuote(quote) => Some(handle_quote(quote, warn)),
-		Node::FootnoteDefinition(_) => warn_error("FootnoteDefinition", warn),
-		Node::MdxJsxFlowElement(_) => warn_error("MdxJsFlowElement", warn),
+		Node::FootnoteDefinition(_) => handle_warning("FootnoteDefinition", warn),
+		Node::MdxJsxFlowElement(_) => handle_warning("MdxJsFlowElement", warn),
 		Node::List(_) => todo!(),
-		Node::MdxjsEsm(_) => warn_error("MdxjsEsm", warn),
-		Node::Toml(_) => warn_error("Toml", warn),
-		Node::Yaml(_) => warn_error("Yaml", warn),
-		Node::Break(_) => Some("\n".into()),
+		Node::MdxjsEsm(_) => handle_warning("MdxjsEsm", warn),
+		Node::Toml(_) => handle_warning("Toml", warn),
+		Node::Yaml(_) => handle_warning("Yaml", warn),
+		Node::Break(_) => Some(handle_break()),
 		Node::InlineCode(_) => todo!(),
 		Node::InlineMath(_) => todo!(),
 		Node::Delete(_) => todo!(),
 		Node::Emphasis(_) => todo!(),
-		Node::MdxTextExpression(_) => warn_error("MdxTextExpression", warn),
-		Node::FootnoteReference(_) => warn_error("FootnoteReference", warn),
-		Node::Html(_) => warn_error("HTML", warn),
+		Node::MdxTextExpression(_) => handle_warning("MdxTextExpression", warn),
+		Node::FootnoteReference(_) => handle_warning("FootnoteReference", warn),
+		Node::Html(_) => handle_warning("HTML", warn),
 		Node::Image(_) => todo!(),
 		Node::ImageReference(_) => todo!(),
-		Node::MdxJsxTextElement(_) => warn_error("MdxJsxTextElement", warn),
+		Node::MdxJsxTextElement(_) => handle_warning("MdxJsxTextElement", warn),
 		Node::Link(_) => todo!(),
 		Node::LinkReference(_) => todo!(),
 		Node::Strong(_) => todo!(),
 		Node::Text(text) => Some(text.value.clone()),
 		Node::Code(_) => todo!(),
 		Node::Math(_) => todo!(),
-		Node::MdxFlowExpression(_) => warn_error("MdxFlowExpression", warn),
+		Node::MdxFlowExpression(_) => handle_warning("MdxFlowExpression", warn),
 		Node::Heading(heading) => Some(handle_heading(heading, warn)),
-		Node::Table(_) => warn_error("Table", warn),
+		Node::Table(_) => handle_warning("Table", warn),
 		Node::ThematicBreak(_) => Some(handle_thematic_break()),
-		Node::TableRow(_) => warn_error("TableRow", warn),
-		Node::TableCell(_) => warn_error("TableCell", warn),
+		Node::TableRow(_) => handle_warning("TableRow", warn),
+		Node::TableCell(_) => handle_warning("TableCell", warn),
 		Node::ListItem(_) => todo!(),
 		Node::Definition(_) => todo!(),
 		Node::Paragraph(paragraph) => Some(handle_paragraph(paragraph, warn)),
 	}
 }
 
-fn warn_error(token: &str, error: &ErrorType) -> Option<String> {
+fn handle_warning(token: &str, error: &WarningType) -> Option<String> {
 	match error {
-		ErrorType::Warn => {
+		WarningType::_Fail => {
+			panic!("WARNING: unsupported syntax found: {}", token)
+		}
+		WarningType::_Silent => None,
+		WarningType::Warn => {
 			eprintln!("WARNING: unsupported syntax skipped: {}", token);
 			None
-		}
-		ErrorType::Fail => {
-			panic!("WARNING: unsupported syntax found: {}", token)
 		}
 	}
 }
 
-fn handle_child_nodes(nodes: &[Node], warn: &ErrorType, separator: &str) -> String {
+fn handle_child_nodes(nodes: &[Node], warn: &WarningType, separator: &str) -> String {
 	nodes
 		.iter()
 		.map(|node| md_to_bbcode(node, warn).unwrap())
@@ -97,11 +99,11 @@ fn handle_child_nodes(nodes: &[Node], warn: &ErrorType, separator: &str) -> Stri
 		.join(separator)
 }
 
-fn handle_root(root: &Root, warn: &ErrorType) -> String {
+fn handle_root(root: &Root, warn: &WarningType) -> String {
 	handle_child_nodes(&root.children, warn, "")
 }
 
-fn handle_quote(blockquote: &BlockQuote, warn: &ErrorType) -> String {
+fn handle_quote(blockquote: &BlockQuote, warn: &WarningType) -> String {
 	let quote = handle_child_nodes(&blockquote.children, warn, "");
 	format!("[quote]{quote}[/quote]\n\n")
 }
@@ -110,7 +112,7 @@ fn handle_break() -> String {
 	"[hr]".into()
 }
 
-fn handle_heading(heading: &Heading, warn: &ErrorType) -> String {
+fn handle_heading(heading: &Heading, warn: &WarningType) -> String {
 	let text = handle_child_nodes(&heading.children, warn, "");
 	format!("[h{l}]{text}[/h{l}]\n", l = heading.depth)
 }
@@ -119,6 +121,6 @@ fn handle_thematic_break() -> String {
 	"[hr]".into()
 }
 
-fn handle_paragraph(paragraph: &Paragraph, warn: &ErrorType) -> String {
+fn handle_paragraph(paragraph: &Paragraph, warn: &WarningType) -> String {
 	handle_child_nodes(&paragraph.children, warn, "\n")
 }
