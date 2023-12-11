37adda5865c82eebd779f6a78c7591b4784fe93a
Silk Rose <silkrose@love-tolerance.com>
2023-12-10 22:57:10 -0500

    Rarity update print error to take str reference

--- code/src/stderr.rs
+++ code/src/stderr.rs
@@ -5,12 +5,12 @@ pub enum ErrColor {
 	Red,
 }
 
-pub fn print_error(message: String, color: ErrColor) {
+pub fn print_error(message: &str, color: ErrColor) {
 	let error = match color {
 		ErrColor::Yellow => tcb::new().fg().yellow(),
 		ErrColor::Red => tcb::new().fg().red(),
 	}
-	.text(message)
+	.text(message.to_string())
 	.print();
 	println!("{error}");
 }
