b964f94c7e166517ba8559626affc35da894ff6b
Silk Rose <silkrose@love-tolerance.com>
2024-01-29 17:52:46 -0500

    Update create story folder shell script

--- code/create-story-folder.sh
+++ code/create-story-folder.sh
@@ -18,7 +18,7 @@ printf "%s\n\n" "# $story_name" > "../stories/$path_name/$path_name.md"
 
 sed -e "s/# Title/# $story_name/g" \
 	-e "/## Story:/!b;n;c\\[$story_name](.\\/$path_name.md)" \
-	< "../markdown-templates/story-one-shot.md" > "../stories/$path_name/$path_name-meta.md"
+	< "../archive/markdown-templates/story-one-shot.md" > "../stories/$path_name/$path_name-meta.md"
 
 if [ -n "$synopsis" ]; then
 	sed -i "/## Synopsis:/!b;n;c\\$synopsis" "../stories/$path_name/$path_name-meta.md"
