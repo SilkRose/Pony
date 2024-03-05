8bb4ad387111e83327bb906e34cd32027e8804e9
Silk Rose <silkrose@love-tolerance.com>
2024-03-05 18:27:59 -0500

    Add API requests to fimfic stats script

--- code/fimfic-stats/fimfic-stats.ts
+++ code/fimfic-stats/fimfic-stats.ts
@@ -8,12 +8,35 @@ import fs from "fs";
 await mane();
 
 async function mane() {
-	const domain = "https://www.fimfiction.net/story/stats";
+	const access_token = process.argv[2];
+	const api_domain = "https://www.fimfiction.net/api/v2/stories";
+	const stats_domain = "https://www.fimfiction.net/story/stats";
 	for (let id = 551751; 1 < 552652; id++) {
-		const url = `${domain}/${id}`;
-		const html = await fetch(url).then((res) => res.text());
-		if (!html.includes('data-controller="story-stats"')) continue;
-		const stats = html
+		let api_status = 200;
+		const api_json = await fetch(`${api_domain}/${id}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${access_token}`,
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			if (!response.ok) {
+				api_status = response.status;
+				console.error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		});
+		if (api_status === 429) {
+			sleep(5000);
+			id = id - 1;
+			continue;
+		}
+		const stats_html = await fetch(`${stats_domain}/${id}`).then((res) =>
+			res.text(),
+		);
+		console.log(api_json);
+		if (!stats_html.includes('data-controller="story-stats"')) continue;
+		const stats = stats_html
 			.split("\n")
 			.filter((l) =>
 				l.startsWith('<div class="layout-two-columns story-stats"'),
@@ -26,4 +49,12 @@ async function mane() {
 			.replaceAll("&quot;", '"');
 		console.log(id, JSON.parse(stats));
 	}
+}
+
+function sleep(milliseconds: number) {
+	const date = Date.now();
+	let current_date = null;
+	do {
+		current_date = Date.now();
+	} while (current_date - date < milliseconds);
 }
