2e375b876397f7027abddbe6038b2823f2e222b0
Silk Rose <silkrose@love-tolerance.com>
2024-03-05 21:30:02 -0500

    Fimfic stats cleanup, add comments

--- code/fimfic-stats/fimfic-stats.ts
+++ code/fimfic-stats/fimfic-stats.ts
@@ -1,18 +1,21 @@
 #!/usr/bin/env bun
 
 import "@total-typescript/ts-reset";
-import { NodeHtmlMarkdown } from "node-html-markdown";
 import * as plib from "./lib.ts";
 import fs from "fs";
 
 await mane();
 
 async function mane() {
+	// API Bearer token is required to scrape the data.
 	const access_token = process.argv[2];
 	const api_domain = "https://www.fimfiction.net/api/v2/stories";
 	const stats_domain = "https://www.fimfiction.net/story/stats";
-	for (let id = 551751; 1 < 552652; id++) {
+	// Loop over IDs to scrape data.
+	for (let id = 551751; id < 552652; id++) {
+		// Set API status to 200.
 		let api_status = 200;
+		// Get data from the API.
 		const api_json = await fetch(`${api_domain}/${id}`, {
 			method: "GET",
 			headers: {
@@ -26,16 +29,26 @@ async function mane() {
 			}
 			return response.json();
 		});
+		// Check for rate limiting.
 		if (api_status === 429) {
 			sleep(5000);
 			id = id - 1;
 			continue;
 		}
+		console.log(api_json);
+		// Get html of the stats page.
 		const stats_html = await fetch(`${stats_domain}/${id}`).then((res) =>
 			res.text(),
 		);
-		console.log(api_json);
+		// Checks to see if the story is deleted or unpublished.
+		if (!stats_html.includes('data-controller="story-stats"') && api_status === 404) {
+			console.warn("deleted story")
+		}
+		if (stats_html.includes('data-controller="story-stats"') && api_status === 404) {
+			console.warn("unpublished story")
+		}
 		if (!stats_html.includes('data-controller="story-stats"')) continue;
+		// Format the historical data into JSON.
 		const stats = stats_html
 			.split("\n")
 			.filter((l) =>
@@ -47,7 +60,9 @@ async function mane() {
 			)
 			.replace(/">$/, "")
 			.replaceAll("&quot;", '"');
+		
 		console.log(id, JSON.parse(stats));
+		sleep(1000);
 	}
 }
 
