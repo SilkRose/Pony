0388027614a6b7586500dcf43f99288e7ebb9236
Silk Rose <silkrose@love-tolerance.com>
2024-03-14 07:20:02 -0400

    FIMFiction April Fools update
    Updated chapters struct to events, added filter for events based off time. Added handling bad responses from API.

--- code/src/bin/fimfic-af.rs
+++ code/src/bin/fimfic-af.rs
@@ -1,27 +1,68 @@
+use async_recursion::async_recursion;
 use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
 use reqwest::Client;
 use serde_json::{json, Value};
 use std::env;
+use std::time::Duration;
 use wiwi::clock_timer::{now, ClockTimer, TimeDelta, Timelike, SECS_PER_DAY};
 
-struct Chapter {
-	id: u32,
-	release_time: u32,
-	description: Option<String>,
-	short_description: Option<String>,
+struct Event<'a> {
+	release_hour: u32,
+	release_minute: u32,
+	chapter_id: Option<u32>,
+	description: Option<&'a str>,
+	short_description: Option<&'a str>,
 }
 
-const CHAPTERS: &[Chapter] = &[Chapter {
-	id: 10,
-	release_time: 10,
-	description: None,
-	short_description: None,
-}];
+const EVENTS: &[Event] = &[
+	Event {
+		release_hour: 13,
+		release_minute: 40,
+		chapter_id: Some(1738301),
+		description: Some(
+			"I agree you, Pinkie is super cute!\n\nI love to give Pinkie lots of hugs!",
+		),
+		short_description: Some("Pinkie is cute!"),
+	},
+	Event {
+		release_hour: 13,
+		release_minute: 40,
+		chapter_id: Some(1738302),
+		description: Some(
+			"I agree you, Fluttershy is super cute!\n\nI love to give Fluttershy lots of hugs!",
+		),
+		short_description: Some("Fluttershy is cute!"),
+	},
+	Event {
+		release_hour: 13,
+		release_minute: 40,
+		chapter_id: Some(1738303),
+		description: Some(
+			"I agree you, Rarity is super cute!\n\nI love to give Rarity lots of hugs!",
+		),
+		short_description: None,
+	},
+	Event {
+		release_hour: 13,
+		release_minute: 40,
+		chapter_id: Some(1738304),
+		description: None,
+		short_description: Some("Twilight Sparkle is cute!"),
+	},
+	Event {
+		release_hour: 13,
+		release_minute: 40,
+		chapter_id: None,
+		description: None,
+		short_description: Some("Rainbow Dash is cute!"),
+	},
+];
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let story_id = 552650;
-	let api_domain = "https://www.fimfiction.net/api/v2/stories";
+	let stories_domain = "https://www.fimfiction.net/api/v2/stories";
+	let chapters_domain = "https://www.fimfiction.net/api/v2/chapters";
 
 	let token = &env::args().collect::<Vec<_>>()[1];
 	let client = Client::new();
@@ -33,7 +74,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	);
 	headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
 
-	let api_url = format!("{api_domain}/{story_id}");
+	let api_url = format!("{stories_domain}/{story_id}");
 
 	let mut timer = ClockTimer::with_naive_datetime(now());
 	timer.set_run_duration(TimeDelta::try_seconds(SECS_PER_DAY as _).unwrap());
@@ -58,26 +99,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		let title = format!(
 			"This Story will Explode in {remaining_hours} Hours and {remaining_mins} Minutes"
 		);
-		let story_json = story_json(story_id, title, None, None).to_string();
-		//let body = chapter_json(story_id).to_string();
-		println!("{story_json}");
-		let _ = send_api_request(&client, &headers, &api_url, story_json).await;
+		let mut desc: Option<&str> = None;
+		let mut short_desc: Option<&str> = None;
+		let event = EVENTS
+			.iter()
+			.filter(|event| {
+				event.release_hour == remaining_hours && event.release_minute == remaining_mins
+			})
+			.collect::<Vec<_>>();
+
+		let story_json = story_json(story_id, title, desc, short_desc).to_string();
+		let _ = send_api_request(&client, &headers, &api_url, story_json, 0).await;
 	}
 
 	Ok(())
 }
 
+#[async_recursion]
 async fn send_api_request(
-	client: &Client, headers: &HeaderMap, url: &String, body: String,
+	client: &Client, headers: &HeaderMap, url: &String, body: String, recursion_level: u32,
 ) -> Result<(), Box<dyn std::error::Error>> {
 	let api_response = client
 		.patch(url)
 		.headers(headers.clone())
-		.body(body)
+		.body(body.clone())
 		.send()
 		.await?;
-	let api = api_response.text().await;
-	println!("{:#?}", api);
+	if !api_response.status().is_success() {
+		if recursion_level > 4 {
+			eprintln!("Failed to send API request five times!\n{url}\n{body}");
+			return Ok(());
+		}
+		tokio::time::sleep(Duration::from_millis(5000)).await;
+		return send_api_request(client, headers, url, body, recursion_level + 1).await;
+	}
 	Ok(())
 }
 
@@ -93,7 +148,7 @@ fn chapter_json(id: u32) -> Value {
 }
 
 fn story_json(
-	id: u32, title: String, description: Option<String>, short_description: Option<String>,
+	id: u32, title: String, description: Option<&str>, short_description: Option<&str>,
 ) -> Value {
 	match (description, short_description) {
 		(None, None) => json!({
