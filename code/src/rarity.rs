720547e8334a166b42bf5774df77b0c3ba2e9df3
Silk Rose <silkrose@love-tolerance.com>
2024-04-13 09:33:47 -0400

    Golden Oak Library Rarity code update
    Update word counting code.

--- code/src/rarity.rs
+++ code/src/rarity.rs
@@ -15,16 +15,16 @@ pub fn count_occurances(text: String, word: String) -> usize {
 	text.split_whitespace().filter(|w| w == &word).count()
 }
 
-pub fn count_regex(text: String, word: String, regex: String) {
+pub fn count_regex(text: String, word: String, regex: String) -> Word {
 	let word_regex = Regex::new(&regex).unwrap();
 	let matches = word_regex.find_iter(&text);
 	let chars = matches.map(|m| m.as_str()).collect::<Vec<_>>().join("").chars().count();
-	let word = Word {
+	Word {
 		mane_word: word,
 		word_regex: regex,
 		occurances: word_regex.find_iter(&text).count(),
 		percentage: 0,
 		total_chars: chars,
 		char_percentage: 0,
-	};
+	}
 }
