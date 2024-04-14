35464e6b111c531527d5715b73e935a3d3c492ec
Silk Rose <silkrose@love-tolerance.com>
2024-04-14 11:41:20 -0400

    Run shell script format on fimfic get story script

--- archive/code/shell-scripts/fimfic-get-story.sh
+++ archive/code/shell-scripts/fimfic-get-story.sh
@@ -8,4 +8,4 @@ access_token=$1
 curl --location --request GET \
 	"https://www.fimfiction.net/api/v2/stories/551751" \
 	--header "Authorization: Bearer $access_token" \
-	--header "Content-Type: application/json" \
+	--header "Content-Type: application/json"
