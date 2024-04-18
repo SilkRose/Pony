dd13ddb5b8c3a349125bc0dda8f79fec53e58993
Silk Rose <silkrose@love-tolerance.com>
2024-04-18 05:38:29 -0400

    Twilight Sparkle package code update
    Implementation of word counting.

--- code/packages/twilight-sparkle/src/word_stats.rs
+++ code/packages/twilight-sparkle/src/word_stats.rs
@@ -2,30 +2,30 @@ use crate::{parser, text_stats::TextType};
 use parser::parse;
 use regex::Regex;
 
-pub fn get_word_stats(text: String, text_type: TextType) {
-	let text = match text_type {
-		TextType::Markdown => parse(text),
-		TextType::Plaintext => text,
-	};
-}
-
+#[derive(Debug)]
 pub struct SearchWords {
 	identifier: String,
 	regex: Regex,
 }
 
-pub struct Word {
-	mane_word: String,
-	word_regex: String,
-	occurances: usize,
+#[derive(Debug)]
+pub struct WordResult {
+	identifier: String,
+	count: usize,
 }
 
-pub fn count_regex(text: String, word: String, regex: String) -> Word {
-	let word_regex = Regex::new(&regex).unwrap();
-	let matches = word_regex.find_iter(&text);
-	Word {
-		mane_word: word,
-		word_regex: regex,
-		occurances: word_regex.find_iter(&text).count(),
-	}
+pub fn get_word_stats(
+	text: String, text_type: TextType, words: Vec<SearchWords>,
+) -> Vec<WordResult> {
+	let text = match text_type {
+		TextType::Markdown => parse(text),
+		TextType::Plaintext => text,
+	};
+	words
+		.iter()
+		.map(|word| WordResult {
+			identifier: word.identifier.clone(),
+			count: word.regex.find_iter(&text).count(),
+		})
+		.collect()
 }
