f0955aeae170e3a266555db21342f94a053eac86
Silk Rose <silkrose@love-tolerance.com>
2024-04-19 01:24:44 -0400

    Applejack package code update
    Makes the return values results so the user handles the errors.

--- code/packages/applejack/src/lib.rs
+++ code/packages/applejack/src/lib.rs
@@ -3,18 +3,19 @@
 
 use camino::Utf8Path;
 use regex::Regex;
+use std::io;
 
 /// Find files function, takes in a dir, and Vectors of Regex, for what the returned files must include and exclude.
 pub fn find_files_in_dir(
 	dir: &str, recursive: bool, includes: &Option<Vec<Regex>>, excludes: &Option<Vec<Regex>>,
-) -> Vec<String> {
+) -> io::Result<Vec<String>> {
 	let mut files = vec![];
-	let paths = Utf8Path::read_dir_utf8(dir.into()).unwrap();
+	let paths = Utf8Path::read_dir_utf8(dir.into())?;
 	for path in paths {
-		let path = path.unwrap().path().to_string();
+		let path = path?.path().to_string();
 		let utf8_path = Utf8Path::new(&path);
 		if utf8_path.is_dir() && recursive {
-			files.extend(find_files_in_dir(&path, recursive, includes, excludes));
+			files.extend(find_files_in_dir(&path, recursive, includes, excludes)?);
 		} else if utf8_path.is_file() {
 			if let Some(excludes) = excludes {
 				if excludes.iter().any(|forbidden| forbidden.is_match(&path)) {
@@ -31,17 +32,17 @@ pub fn find_files_in_dir(
 			}
 		}
 	}
-	files
+	Ok(files)
 }
 
 /// Find dirs function, takes in a dir, and Vectors of Regex, for what the returned dirs must include and exclude.
 pub fn find_dirs_in_dir(
 	dir: &str, recursive: bool, includes: &Option<Vec<Regex>>, excludes: &Option<Vec<Regex>>,
-) -> Vec<String> {
+) -> io::Result<Vec<String>> {
 	let mut dirs = vec![];
-	let paths = Utf8Path::read_dir_utf8(dir.into()).unwrap();
+	let paths = Utf8Path::read_dir_utf8(dir.into())?;
 	for path in paths {
-		let path = path.unwrap().path().to_string();
+		let path = path?.path().to_string();
 		let utf8_path = Utf8Path::new(&path);
 		if utf8_path.is_dir() {
 			if let Some(excludes) = excludes {
@@ -57,10 +58,10 @@ pub fn find_dirs_in_dir(
 			} else {
 				dirs.push(path);
 				if recursive {
-					dirs.extend(find_dirs_in_dir(dir, recursive, includes, excludes))
+					dirs.extend(find_dirs_in_dir(dir, recursive, includes, excludes)?)
 				}
 			}
 		}
 	}
-	dirs
+	Ok(dirs)
 }
