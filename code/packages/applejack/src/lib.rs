00a3a83b5aab3c9b47260685cc46769a3bbaea82
Silk Rose <silkrose@love-tolerance.com>
2024-04-19 10:41:02 -0400

    Applejack package code update
    Add fn filter module. These functions take a function for filtering.

--- code/packages/applejack/src/lib.rs
+++ code/packages/applejack/src/lib.rs
@@ -1,6 +1,9 @@
 #![deny(missing_docs)]
 #![doc = include_str!("../readme.md")]
 
+/// Module for function with function filters.
+pub mod fn_filter;
+
 /// Module for functions with no filters.
 pub mod no_filter;
 
