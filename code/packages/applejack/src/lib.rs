78d4e699c5a5a0c3fa4656c79ee104b5e9fda6e5
Silk Rose <silkrose@love-tolerance.com>
2024-04-19 05:23:07 -0400

    Applejack package code update
    Add no filter module and functions.

--- code/packages/applejack/src/lib.rs
+++ code/packages/applejack/src/lib.rs
@@ -1,5 +1,8 @@
 #![deny(missing_docs)]
 #![doc = include_str!("../readme.md")]
 
+/// Module for functions with no filters.
+pub mod no_filter;
+
 /// Module for functions with Regex filtering.
 pub mod regex_filter;
