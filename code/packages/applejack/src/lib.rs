b8ee660a8a2205a1f2a9b32d91b17e6dd259f515
Silk Rose <silkrose@love-tolerance.com>
2024-04-19 23:39:19 -0400

    Applejack package code update
    Closure filters will be the only option, reducing duplicate code.

--- code/packages/applejack/src/lib.rs
+++ code/packages/applejack/src/lib.rs
@@ -1,11 +1,58 @@
 #![deny(missing_docs)]
 #![doc = include_str!("../readme.md")]
 
-/// Module for function with function filters.
-pub mod fn_filter;
+use camino::Utf8Path;
+use std::io;
 
-/// Module for functions with no filters.
-pub mod no_filter;
+/// Find files function, takes in a dir, and a function for what to include / exclude.
+pub fn find_files_in_dir<F>(
+	dir: &str, recursive: bool, filter: Option<F>,
+) -> io::Result<Vec<String>>
+where
+	F: FnMut(&str) -> bool + std::marker::Copy,
+{
+	let mut files = vec![];
+	let paths = Utf8Path::read_dir_utf8(dir.into())?;
+	for path in paths {
+		let path = path?.path().to_string();
+		let utf8_path = Utf8Path::new(&path);
+		if utf8_path.is_dir() && recursive {
+			files.extend(find_files_in_dir(&path, recursive, filter)?);
+		} else if utf8_path.is_file() {
+			if let Some(mut filter) = filter {
+				if filter(&path) {
+					files.push(path);
+				}
+			} else {
+				files.push(path);
+			}
+		}
+	}
+	Ok(files)
+}
 
-/// Module for functions with Regex filtering.
-pub mod regex_filter;
+/// Find dirs function, takes in a dir, and a function for what to include / exclude.
+pub fn find_dirs_in_dir<F>(dir: &str, recursive: bool, filter: Option<F>) -> io::Result<Vec<String>>
+where
+	F: FnMut(&str) -> bool + std::marker::Copy,
+{
+	let mut dirs = vec![];
+	let paths = Utf8Path::read_dir_utf8(dir.into())?;
+	for path in paths {
+		let path = path?.path().to_string();
+		let utf8_path = Utf8Path::new(&path);
+		if utf8_path.is_dir() {
+			if let Some(mut filter) = filter {
+				if filter(&path) {
+					dirs.push(path);
+					if recursive {
+						dirs.extend(find_dirs_in_dir(dir, recursive, Some(filter))?)
+					}
+				}
+			} else {
+				dirs.push(path);
+			}
+		}
+	}
+	Ok(dirs)
+}
