cf3ca47617f4e49d9cd7251e8de6a968c482604c
Silk Rose <silkrose@love-tolerance.com>
2024-04-20 02:19:06 -0400

    Fluttershy package code update
    Add retry function for retrying functions a set number of times.
    @meadowsys came up with the idea and offered improvements after the fact.

--- code/packages/fluttershy/src/lib.rs
+++ code/packages/fluttershy/src/lib.rs
@@ -1,3 +1,15 @@
-pub fn dash() {
-	println!("Rainbow Dash");
+use std::error::Error;
+
+pub fn retry<T, F>(attempts: usize, function: F) -> Result<T, Box<dyn Error>>
+where
+	F: Fn() -> Result<T, Box<dyn Error>>,
+{
+	let mut attempt = function();
+	for _ in 1..attempts {
+		if attempt.is_ok() {
+			return attempt;
+		}
+		attempt = function();
+	}
+	attempt
 }
