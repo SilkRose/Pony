0ad16d0429466bfbb701718abf71a4f309c2fe24
Silk Rose <silkrose@love-tolerance.com>
2024-04-20 03:58:51 -0400

    Fluttershy package code update
    Change attempts in retry to nonzero usize. Add retry forever function.

--- code/packages/fluttershy/src/lib.rs
+++ code/packages/fluttershy/src/lib.rs
@@ -1,15 +1,29 @@
 use std::error::Error;
+use std::num::NonZeroUsize;
 
-pub fn retry<T, F>(attempts: usize, function: F) -> Result<T, Box<dyn Error>>
+pub fn retry<T, F>(attempts: NonZeroUsize, function: F) -> Result<T, Box<dyn Error>>
 where
 	F: Fn() -> Result<T, Box<dyn Error>>,
 {
 	let mut attempt = function();
-	for _ in 1..attempts {
+	for _ in 1..attempts.get() {
 		if attempt.is_ok() {
 			return attempt;
 		}
 		attempt = function();
 	}
 	attempt
+}
+
+pub fn retry_forever<T, F>(function: F) -> Result<T, Box<dyn Error>>
+where
+	F: Fn() -> Result<T, Box<dyn Error>>,
+{
+	let mut attempt = function();
+	loop {
+		if attempt.is_ok() {
+			return attempt;
+		}
+		attempt = function();
+	}
 }
