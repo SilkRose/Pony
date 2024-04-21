955396185cfae6b651f825d1262842eaeb83fdff
Silk Rose <silkrose@love-tolerance.com>
2024-04-21 05:17:35 -0400

    Fluttershy package code update
    Add a more advanced retry function and a sleep function.
    Plan to move the sleep function at some point into Rainbow Dash package.

--- code/packages/fluttershy/src/lib.rs
+++ code/packages/fluttershy/src/lib.rs
@@ -1,7 +1,37 @@
 use std::error::Error;
 use std::num::NonZeroUsize;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-pub fn retry<T, F>(attempts: NonZeroUsize, function: F) -> Result<T, Box<dyn Error>>
+/// The initial duration in milliseconds to wait and the multiplier for each subsequent run.
+pub type Backoff = (u128, f64);
+
+pub fn retry<T, F>(
+	mut attempts: Option<usize>, mut backoff: Option<Backoff>, function: F,
+) -> Result<T, Box<dyn Error>>
+where
+	F: Fn() -> Result<T, Box<dyn Error>>,
+{
+	let mut attempt;
+	loop {
+		let start_time = SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis();
+		attempt = function();
+		if attempt.is_ok() {
+			return attempt;
+		}
+		if attempts.is_some() {
+			attempts = Some(attempts.unwrap() - 1);
+			if attempts.unwrap() == 0 {
+				return attempt;
+			}
+		}
+		if let Some((initial_delay, multiplier)) = backoff {
+			sleep(start_time, initial_delay);
+			backoff = Some(((initial_delay as f64 * multiplier) as u128, multiplier));
+		}
+	}
+}
+
+pub fn retry_max_tries<T, F>(attempts: NonZeroUsize, function: F) -> Result<T, Box<dyn Error>>
 where
 	F: Fn() -> Result<T, Box<dyn Error>>,
 {
@@ -26,4 +56,17 @@ where
 		}
 		attempt = function();
 	}
+}
+
+fn sleep(start_time: u128, interval: u128) {
+	let current_time = SystemTime::now()
+		.duration_since(UNIX_EPOCH)
+		.unwrap()
+		.as_millis();
+	let elapsed_time = current_time - start_time;
+	println!("{elapsed_time}");
+	if elapsed_time > interval {
+		return;
+	};
+	std::thread::sleep(Duration::from_millis((interval - elapsed_time) as u64));
 }
