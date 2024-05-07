7d824cfd83f9c91e5a19bbcf9f6843c0e31d8c55
Silk Rose <silkrose@love-tolerance.com>
2024-05-07 05:55:12 -0400

    Pony package code update
    UI design for create story folder binary, got the UI to the same point as the old shell script. Now to just add the functionality.

--- code/src/bin/create-story-folder.rs
+++ code/src/bin/create-story-folder.rs
@@ -1,5 +1,8 @@
-use gtk::{glib, prelude::*, Application, ApplicationWindow, Box, Button, Entry, Orientation};
-const APP_ID: &str = "dev.silkrose.test-ui";
+use gtk::{
+	glib, prelude::*, Application, ApplicationWindow, Box, Button, Entry, Label, Orientation,
+	TextBuffer, TextView,
+};
+const APP_ID: &str = "dev.silkrose.create-story-folder";
 
 fn main() -> glib::ExitCode {
 	let app = Application::builder().application_id(APP_ID).build();
@@ -10,16 +13,38 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
+	let title_label = Label::builder().label("Title:").margin_top(12).build();
+
 	let title = Entry::builder()
 		.max_length(80)
-		.margin_top(24)
+		.margin_top(12)
+		.margin_bottom(12)
+		.margin_start(12)
+		.margin_end(12)
+		.build();
+
+	let synopsis_label = Label::builder().label("Synopsis:").build();
+
+	let synopsis_text = TextBuffer::builder().build();
+	let synopsis = TextView::builder()
+		.buffer(&synopsis_text)
+		.pixels_above_lines(2)
+		.pixels_below_lines(2)
+		.pixels_inside_wrap(2)
+		.height_request(66)
+		.bottom_margin(4)
+		.top_margin(4)
+		.left_margin(4)
+		.right_margin(4)
+		.wrap_mode(gtk::WrapMode::Word)
+		.margin_top(12)
 		.margin_bottom(12)
 		.margin_start(12)
 		.margin_end(12)
 		.build();
 
 	let button = Button::builder()
-		.label("Button")
+		.label("Create Story Folder")
 		.margin_top(12)
 		.margin_bottom(12)
 		.margin_start(12)
@@ -31,14 +56,17 @@ fn build_ui(app: &Application) {
 	});
 
 	let content = Box::new(Orientation::Vertical, 0);
+	content.append(&title_label);
 	content.append(&title);
+	content.append(&synopsis_label);
+	content.append(&synopsis);
 	content.append(&button);
 
 	let window = ApplicationWindow::builder()
 		.application(app)
-		.title("My GTK App")
-		.width_request(320)
-		.height_request(240)
+		.title("Create Story Folder")
+		.width_request(400)
+		.height_request(200)
 		.child(&content)
 		.build();
 
