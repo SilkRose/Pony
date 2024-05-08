8023ee0c41ad9e1bcb71f2fd0e9bb492a66e9613
Silk Rose <silkrose@love-tolerance.com>
2024-05-07 23:05:01 -0400

    Pony package code update
    Add the functionality for parity with the old shell script.

--- code/src/bin/create-story-folder.rs
+++ code/src/bin/create-story-folder.rs
@@ -2,6 +2,9 @@ use gtk::{
 	glib, prelude::*, Application, ApplicationWindow, Box, Button, Entry, Label, Orientation,
 	TextBuffer, TextView,
 };
+use std::fs;
+
+const TEMPLATE: &str = include_str!("../../../archive/markdown-templates/story-one-shot.md");
 const APP_ID: &str = "dev.silkrose.create-story-folder";
 
 fn main() -> glib::ExitCode {
@@ -51,16 +54,21 @@ fn build_ui(app: &Application) {
 		.margin_end(12)
 		.build();
 
-	button.connect_clicked(|button| {
-		button.set_label("Test");
-	});
-
 	let content = Box::new(Orientation::Vertical, 0);
 	content.append(&title_label);
 	content.append(&title);
 	content.append(&synopsis_label);
 	content.append(&synopsis);
 	content.append(&button);
+
+	button.connect_clicked(move |_| {
+		create_story_folder(
+			title.text().as_ref(),
+			synopsis_text
+				.text(&synopsis_text.start_iter(), &synopsis_text.end_iter(), true)
+				.as_ref(),
+		)
+	});
 
 	let window = ApplicationWindow::builder()
 		.application(app)
@@ -71,4 +79,24 @@ fn build_ui(app: &Application) {
 		.build();
 
 	window.present();
+}
+
+fn create_story_folder(title: &str, synopsis: &str) {
+	let filename = title.to_ascii_lowercase().replace(' ', "-");
+	let md = TEMPLATE
+		.replace("# Title", format!("# {title}").as_str())
+		.replace(
+			"## Synopsis:\n\n",
+			format!("## Synopsis:\n{synopsis}\n").as_str(),
+		)
+		.replace(
+			"## Story:\n\n",
+			format!("## Story:\n[{title}](./{filename}.md)\n").as_str(),
+		);
+	let dir = format!("../stories/{filename}");
+	fs::create_dir(dir.clone()).unwrap();
+	let story = format!("{dir}/{filename}.md");
+	let meta = format!("{dir}/{filename}-meta.md");
+	fs::write(story, format!("# {title}\n\n")).unwrap();
+	fs::write(meta, md).unwrap();
 }
