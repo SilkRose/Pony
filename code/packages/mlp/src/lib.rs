45e7e25f8b4e2b0f22a76c97c4d4871399bc7902
Silk Rose <silkrose@love-tolerance.com>
2024-05-09 03:12:11 -0400

    My Little Pagination package code update
    Code written by @meadowsys.

--- code/packages/mlp/src/lib.rs
+++ code/packages/mlp/src/lib.rs
@@ -1,1 +1,37 @@
+use std::{num::NonZeroUsize, slice};
 
+fn get_pages<T>(items: &[T], page_size: NonZeroUsize) -> Vec<&[T]> {
+	items.chunks(page_size.get()).collect()
+}
+
+fn get_page<T>(items: &[T], page_size: NonZeroUsize, page: NonZeroUsize) -> Option<&[T]> {
+	let items_start = page_size.get() * page.get();
+	let items_end = page_size.get() * (page.get() + 1);
+
+	if items_start >= items.len() {
+		return None;
+	}
+
+	Some(if items_end > items.len() {
+		&items[items_start..]
+	} else {
+		&items[items_start..items_end]
+	})
+}
+
+fn get_page_with_unsafe_code<T>(
+	items: &[T], page_size: NonZeroUsize, page: NonZeroUsize,
+) -> Option<&[T]> {
+	let items_start = page_size.get() * page.get();
+
+	if items_start >= items.len() {
+		return None;
+	}
+
+	let num_items = usize::min(page_size.get(), items.len() - items_start);
+	unsafe {
+		let ptr = items as *const [T] as *const T;
+		let slice = slice::from_raw_parts(ptr.add(items_start), num_items);
+		Some(slice)
+	}
+}
