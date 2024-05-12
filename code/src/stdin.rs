c6981955f433f0400555ac641e41d8d45edcfdc9
Silk Rose <silkrose@love-tolerance.com>
2024-05-12 02:01:08 -0400

    Pony package update
    Add ask function to stdin.

--- code/src/stdin.rs
+++ code/src/stdin.rs
@@ -1,5 +1,6 @@
 use atty::Stream;
-use std::io;
+use std::error::Error;
+use std::io::{self, Write};
 
 /// Function to collect stdin or return None.
 pub fn get_stdin() -> Option<String> {
@@ -10,4 +11,21 @@ pub fn get_stdin() -> Option<String> {
 			.collect::<Vec<_>>()
 			.join("\n")
 	})
+}
+
+pub fn ask<F>(question: &str, filter: Option<(F, &str)>) -> Result<String, Box<dyn Error>>
+where
+	F: Fn(&str) -> bool,
+{
+	let mut answer = String::new();
+	println!("{}", question);
+	io::stdout().flush()?;
+	io::stdin().read_line(&mut answer)?;
+	if let Some((filter_fn, filter_err)) = filter {
+		if !filter_fn(answer.trim()) {
+			eprintln!("{filter_err}");
+			return ask(question, Some((filter_fn, filter_err)));
+		}
+	}
+	Ok(answer.trim().to_string())
 }
