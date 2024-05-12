8cb9569db29bcae35682f138d223e68b9feb56b6
Silk Rose <silkrose@love-tolerance.com>
2024-05-12 03:04:52 -0400

    Pony package update
    Removes GTK4 code and uses ask function from Pony crate.

--- code/src/bin/create-story-folder.rs
+++ code/src/bin/create-story-folder.rs
@@ -1,84 +1,19 @@
-use gtk::{
-	glib, prelude::*, Application, ApplicationWindow, Box, Button, Entry, Label, Orientation,
-	TextBuffer, TextView,
-};
-use std::fs;
+use pony::stdin::ask;
+use std::{error::Error, fs};
 
 const TEMPLATE: &str = include_str!("../../../archive/markdown-templates/story-one-shot.md");
-const APP_ID: &str = "dev.silkrose.create-story-folder";
 
-fn main() -> glib::ExitCode {
-	let app = Application::builder().application_id(APP_ID).build();
-
-	app.connect_activate(build_ui);
-
-	app.run()
-}
-
-fn build_ui(app: &Application) {
-	let title_label = Label::builder().label("Title:").margin_top(12).build();
-
-	let title = Entry::builder()
-		.max_length(80)
-		.margin_top(12)
-		.margin_bottom(12)
-		.margin_start(12)
-		.margin_end(12)
-		.build();
-
-	let synopsis_label = Label::builder().label("Synopsis:").build();
-
-	let synopsis_text = TextBuffer::builder().build();
-	let synopsis = TextView::builder()
-		.buffer(&synopsis_text)
-		.pixels_above_lines(2)
-		.pixels_below_lines(2)
-		.pixels_inside_wrap(2)
-		.height_request(66)
-		.bottom_margin(4)
-		.top_margin(4)
-		.left_margin(4)
-		.right_margin(4)
-		.wrap_mode(gtk::WrapMode::Word)
-		.margin_top(12)
-		.margin_bottom(12)
-		.margin_start(12)
-		.margin_end(12)
-		.build();
-
-	let button = Button::builder()
-		.label("Create Story Folder")
-		.margin_top(12)
-		.margin_bottom(12)
-		.margin_start(12)
-		.margin_end(12)
-		.build();
-
-	let content = Box::new(Orientation::Vertical, 0);
-	content.append(&title_label);
-	content.append(&title);
-	content.append(&synopsis_label);
-	content.append(&synopsis);
-	content.append(&button);
-
-	button.connect_clicked(move |_| {
-		create_story_folder(
-			title.text().as_ref(),
-			synopsis_text
-				.text(&synopsis_text.start_iter(), &synopsis_text.end_iter(), true)
-				.as_ref(),
-		)
-	});
-
-	let window = ApplicationWindow::builder()
-		.application(app)
-		.title("Create Story Folder")
-		.width_request(400)
-		.height_request(200)
-		.child(&content)
-		.build();
-
-	window.present();
+fn main() -> Result<(), Box<dyn Error>> {
+	let title = ask(
+		"Story name:",
+		Some((
+			|text: &str| !text.is_empty() && text.len() < 60,
+			"Must not be empty and less than 60 characters.",
+		)),
+	)?;
+	let synopsis = ask("Synopsis:", Some((|_: &str| true, "")))?;
+	create_story_folder(&title, &synopsis);
+	Ok(())
 }
 
 fn create_story_folder(title: &str, synopsis: &str) {
