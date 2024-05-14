1e4e83237d2f13a9e802cf4d1e82440e7e2c618d
Silk Rose <silkrose@love-tolerance.com>
2024-05-14 04:12:13 -0400

    Trot package update
    Add tests.

--- code/packages/trot/src/lib.rs
+++ code/packages/trot/src/lib.rs
@@ -1,3 +1,6 @@
+//#![deny(missing_docs)]
+//#![doc = include_str!("../readme.md")]
+
 use std::cmp::Ordering;
 
 pub trait Vector<T: Ord> {
@@ -34,5 +37,36 @@ pub fn compare<T: PartialOrd>(a: &T, b: &T) -> Ordering {
 		Ordering::Greater
 	} else {
 		Ordering::Equal
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	fn sort() {
+		let vec = vec![2, 1, 1, 2, 0, 3].sort_vec();
+		assert_eq!(vec, vec![0, 1, 1, 2, 2, 3]);
+	}
+	#[test]
+	fn dedup() {
+		let vec = vec![2, 1, 1, 0, 0, 3].dedup_vec();
+		assert_eq!(vec, vec![2, 1, 0, 3]);
+	}
+	#[test]
+	fn sort_and_dedup() {
+		let vec = vec![2, 1, 1, 0, 0, 3].sort_and_dedup_vec();
+		assert_eq!(vec, vec![0, 1, 2, 3]);
+	}
+	#[test]
+	fn extend() {
+		let vec = vec![2, 1, 1, 0, 0, 3].extend_vec(vec![4, 5]);
+		assert_eq!(vec, vec![2, 1, 1, 0, 0, 3, 4, 5]);
+	}
+	#[test]
+	fn compare() {
+		let mut list = vec![(3, 1), (2, 2), (1, 3)];
+		list.sort_by(|a, b| super::compare(&a.0, &b.0));
+		assert_eq!(list, vec![(1, 3), (2, 2), (3, 1)]);
 	}
 }
