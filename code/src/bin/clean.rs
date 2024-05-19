bc0e348349637d377fade18eaa200ea01eed5010
Silk Rose <silkrose@love-tolerance.com>
2024-05-19 05:14:04 -0400

    Pony package update
    Fixes file system module use in the clean and fimd binaries. Also replaces some unwraps with ?

--- code/src/bin/clean.rs
+++ code/src/bin/clean.rs
@@ -1,31 +1,29 @@
 use pony::fs::find_files_in_dir;
 use pony::regex::matches;
 use regex::Regex;
+use std::error::Error;
 use std::fs;
 
-fn main() {
-	let includes = Some(Regex::new(r".*(stories|flash-fiction).*\.md$").unwrap());
-	let excludes = Some(Regex::new(r".*archive.*|(-meta|readme)\.md$").unwrap());
-	let single = Regex::new(r"[‘’´ʹ]").unwrap();
-	let double = Regex::new(r"[“”‟″]").unwrap();
-	find_files_in_dir(
-		"../",
-		true,
-		Some(|path: &_| matches(path, &includes, &excludes)),
-	)
-	.unwrap()
-	.iter()
-	.for_each(|file| {
-		let mut data = fs::read_to_string(file).unwrap();
-		data = single.replace_all(&data, "'").to_string();
-		data = double.replace_all(&data, "\"").to_string();
-		data = data
-			.replace("...", "…")
-			.replace(",*", "*,")
-			.replace(",_", "_,")
-			.replace("---", "—")
-			.replace("--", "–");
+fn main() -> Result<(), Box<dyn Error>> {
+	let includes = Some(Regex::new(r".*(stories|flash-fiction).*\.md$")?);
+	let excludes = Some(Regex::new(r".*archive.*|(-meta|readme)\.md$")?);
+	let single = Regex::new(r"[‘’´ʹ]")?;
+	let double = Regex::new(r"[“”‟″]")?;
+	find_files_in_dir("../", true)?
+		.iter()
+		.filter(|file| matches(file, &includes, &excludes))
+		.for_each(|file| {
+			let mut data = fs::read_to_string(file).unwrap();
+			data = single.replace_all(&data, "'").to_string();
+			data = double.replace_all(&data, "\"").to_string();
+			data = data
+				.replace("...", "…")
+				.replace(",*", "*,")
+				.replace(",_", "_,")
+				.replace("---", "—")
+				.replace("--", "–");
 
-		fs::write(file, data).unwrap();
-	});
+			fs::write(file, data).unwrap();
+		});
+	Ok(())
 }
--- code/src/bin/fimd.rs
+++ code/src/bin/fimd.rs
@@ -3,28 +3,26 @@ use pony::fs::find_files_in_dir;
 use pony::md_to_bbcode::{parse, WarningType};
 use pony::regex::matches;
 use regex::Regex;
+use std::error::Error;
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
 	if Utf8Path::new("./publish").is_dir() {
-		fs::remove_dir_all("./publish").unwrap()
+		fs::remove_dir_all("./publish")?
 	}
-	fs::create_dir("./publish").unwrap();
-	let includes = Some(Regex::new(r".*(stories|flash-fiction).*\.md$").unwrap());
-	let excludes = Some(Regex::new(r".*archive.*|.*(ideas|names|readme)\.md$").unwrap());
-	find_files_in_dir(
-		"../",
-		true,
-		Some(|path: &_| matches(path, &includes, &excludes)),
-	)
-	.unwrap()
-	.iter()
-	.for_each(|input| {
-		let md = fs::read_to_string(input).unwrap();
-		let bbcode = parse(md, &WarningType::Quiet);
-		let output = input.replace("../", "./publish/").replace(".md", ".txt");
-		fs::create_dir_all(Utf8Path::new(&output).parent().unwrap()).unwrap();
-		fs::write(output, bbcode).unwrap();
-		println!("Converted: {input}");
-	});
+	fs::create_dir("./publish")?;
+	let includes = Some(Regex::new(r".*(stories|flash-fiction).*\.md$")?);
+	let excludes = Some(Regex::new(r".*archive.*|.*(ideas|names|readme)\.md$")?);
+	find_files_in_dir("../", true)?
+		.iter()
+		.filter(|file| matches(file, &includes, &excludes))
+		.for_each(|input| {
+			let md = fs::read_to_string(input).unwrap();
+			let bbcode = parse(md, &WarningType::Quiet);
+			let output = input.replace("../", "./publish/").replace(".md", ".txt");
+			fs::create_dir_all(Utf8Path::new(&output).parent().unwrap()).unwrap();
+			fs::write(output, bbcode).unwrap();
+			println!("Converted: {input}");
+		});
+	Ok(())
 }
