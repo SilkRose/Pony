c1125cfc898f071119c00e3fdd2d3b7be69ac883
Silk Rose <silkrose@love-tolerance.com>
2024-05-26 21:07:20 -0400

    Refactor json formatting module

--- code/src/json.rs
+++ code/src/json.rs
@@ -1,46 +1,25 @@
+use super::error::Result;
 use serde::ser::Serialize;
-use serde_json::{ser::PrettyFormatter, Serializer, Value};
+use serde_json::{ser::PrettyFormatter, Serializer};
 
-pub enum Json {
-	Format,
+pub enum JsonFormat {
 	Minify,
-}
-
-pub enum Indent {
 	Tab,
 	Space(u8),
 }
 
-pub fn json_formatter(json_data: String, fmt_type: Json, indent: Indent) -> String {
-	let indent: String = match indent {
-		Indent::Tab => "\t".to_string(),
-		Indent::Space(indent_number) => " ".repeat(indent_number as usize),
-	};
-	match fmt_type {
-		Json::Format => format_json(&json_data, &indent),
-		Json::Minify => minify_json(&json_data),
+pub fn format_json<T: Serialize>(value: &T, format: JsonFormat) -> Result<String> {
+	match format {
+		JsonFormat::Minify => Ok(serde_json::to_string(value)?),
+		JsonFormat::Tab => Ok(format_json_indent(value, "\t")?),
+		JsonFormat::Space(s) => Ok(format_json_indent(value, &" ".repeat(s.into()))?),
 	}
 }
 
-fn format_json(json: &str, indent: &str) -> String {
-	let value = parse_to_value(json);
-	// json.len() is not ideal but its a _goodish_ default
-	let mut writer = Vec::with_capacity(json.len());
+fn format_json_indent<T: Serialize>(value: &T, indent: &str) -> Result<String> {
+	let mut writer = Vec::new();
 	let formatter = PrettyFormatter::with_indent(indent.as_bytes());
-	let mut serialiser = Serializer::with_formatter(&mut writer, formatter);
-	value
-		.serialize(&mut serialiser)
-		.expect("Failed to serialize json data.");
-	writer.push(b'\n');
-	String::from_utf8(writer).expect("Failed to convert utf8 to string.")
-}
-
-fn minify_json(json: &str) -> String {
-	let value = parse_to_value(json);
-	serde_json::to_string(&value).expect("Failed to stringify json.")
-}
-
-#[inline]
-fn parse_to_value(json: &str) -> Value {
-	serde_json::from_str(json).expect("Failed to parse json.")
+	let mut serializer = Serializer::with_formatter(&mut writer, formatter);
+	value.serialize(&mut serializer)?;
+	Ok(String::from_utf8(writer)?)
 }
