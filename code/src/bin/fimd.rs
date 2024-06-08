7b9eb5b880e3b25668c3c6d3cb2e5b0ef5ad58fd
Silk Rose <silkrose@love-tolerance.com>
2024-06-08 04:52:48 -0400

    Update fimd to pass in a string reference

--- code/src/bin/fimd.rs
+++ code/src/bin/fimd.rs
@@ -18,7 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 		.filter(|file| matches(file, &includes, &excludes))
 		.for_each(|input| {
 			let md = fs::read_to_string(input).unwrap();
-			let bbcode = parse(md, &WarningType::Quiet);
+			let bbcode = parse(&md, &WarningType::Quiet);
 			let output = input.replace("../", "./publish/").replace(".md", ".txt");
 			fs::create_dir_all(Utf8Path::new(&output).parent().unwrap()).unwrap();
 			fs::write(output, bbcode).unwrap();
