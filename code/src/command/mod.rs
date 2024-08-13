87c45d6f437be3b500c548476fed593b6f6d895a
Silk Rose <silkrose@love-tolerance.com>
2024-08-13 18:50:51 -0400

    Simplify command module in Pony package
    Instead of six public functions, it is now two. Code coverage is now 100%.

--- code/src/command/mod.rs
+++ code/src/command/mod.rs
@@ -7,37 +7,17 @@ use std::process::{Command, ExitStatus, Output};
 /// Executes a Linux/macOS or Windows command with a Result of ExitStatus.
 pub fn execute_command(cmd: &str) -> Result<ExitStatus> {
 	#[cfg(not(target_os = "windows"))]
-	return execute_unix_command(cmd);
+	return Ok(Command::new("sh").arg("-c").arg(cmd).status()?);
 	#[cfg(target_os = "windows")]
-	return execute_windows_command(cmd);
+	return Ok(Command::new("cmd").args(["/C", cmd]).status()?);
 }
 
 /// Executes a Linux/macOS or Windows command with a Result of the output.
 pub fn execute_command_with_return(cmd: &str) -> Result<Output> {
 	#[cfg(not(target_os = "windows"))]
-	return execute_unix_command_with_return(cmd);
+	return Ok(Command::new("sh").arg("-c").arg(cmd).output()?);
 	#[cfg(target_os = "windows")]
-	return execute_windows_command_with_return(cmd);
-}
-
-/// Executes a Windows command with a Result of ExitStatus.
-pub fn execute_windows_command(cmd: &str) -> Result<ExitStatus> {
-	Ok(Command::new("cmd").args(["/C", cmd]).status()?)
-}
-
-/// Executes a Linux/macOS command with a Result of ExitStatus.
-pub fn execute_unix_command(cmd: &str) -> Result<ExitStatus> {
-	Ok(Command::new("sh").arg("-c").arg(cmd).status()?)
-}
-
-/// Executes a Windows command with a Result of the output.
-pub fn execute_windows_command_with_return(cmd: &str) -> Result<Output> {
-	Ok(Command::new("cmd").args(["/C", cmd]).output()?)
-}
-
-/// Executes a Linux/macOS command with a Result of the output.
-pub fn execute_unix_command_with_return(cmd: &str) -> Result<Output> {
-	Ok(Command::new("sh").arg("-c").arg(cmd).output()?)
+	return Ok(Command::new("cmd").args(["/C", cmd]).output()?);
 }
 
 #[cfg(test)]
@@ -66,28 +46,28 @@ mod tests {
 	#[test]
 	#[cfg(not(target_os = "windows"))]
 	fn unix_status() -> std::result::Result<(), Box<dyn Error>> {
-		let status = execute_unix_command("exit 0")?;
+		let status = execute_command("exit 0")?;
 		assert!(status.success());
 		Ok(())
 	}
 	#[test]
 	#[cfg(not(target_os = "windows"))]
 	fn unix_status_failure() -> std::result::Result<(), Box<dyn Error>> {
-		let status = execute_unix_command("exit 1")?;
+		let status = execute_command("exit 1")?;
 		assert!(!status.success());
 		Ok(())
 	}
 	#[test]
 	#[cfg(target_os = "windows")]
 	fn windows_status() -> std::result::Result<(), Box<dyn Error>> {
-		let status = execute_windows_command("exit 0")?;
+		let status = execute_command("exit 0")?;
 		assert!(status.success());
 		Ok(())
 	}
 	#[test]
 	#[cfg(target_os = "windows")]
 	fn windows_status_failure() -> std::result::Result<(), Box<dyn Error>> {
-		let status = execute_windows_command("exit 1")?;
+		let status = execute_command("exit 1")?;
 		assert!(!status.success());
 		Ok(())
 	}
@@ -95,7 +75,7 @@ mod tests {
 	#[cfg(not(target_os = "windows"))]
 	fn unix_output() -> std::result::Result<(), Box<dyn Error>> {
 		let answer = "Pinkie Pie is best pony!";
-		let output = execute_unix_command_with_return(&format!("echo '{answer}'"))?;
+		let output = execute_command_with_return(&format!("echo '{answer}'"))?;
 		assert_eq!(String::from_utf8(output.stdout)?, format!("{answer}\n"));
 		Ok(())
 	}
@@ -103,7 +83,7 @@ mod tests {
 	#[cfg(target_os = "windows")]
 	fn windows_output() -> std::result::Result<(), Box<dyn Error>> {
 		let answer = "Pinkie Pie is best pony!";
-		let output = execute_windows_command_with_return(&format!("echo '{answer}'"))?;
+		let output = execute_command_with_return(&format!("echo '{answer}'"))?;
 		assert_eq!(String::from_utf8(output.stdout)?, format!("{answer}\n"));
 		Ok(())
 	}
