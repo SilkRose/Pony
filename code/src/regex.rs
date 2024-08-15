87cf79cc569b8d94acb73385e5267dc9d935fa51
Silk Rose <silkrose@love-tolerance.com>
2024-08-15 04:22:29 -0400

    Add seven tests to regex module in Pony

--- code/src/regex.rs
+++ code/src/regex.rs
@@ -11,3 +11,53 @@ pub fn matches(string: &str, includes: &Option<Regex>, excludes: &Option<Regex>)
 	}
 	true
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use regex::Regex;
+
+	#[test]
+	fn none() {
+		let matched = matches("Pinkie Pie is best pony!", &None, &None);
+		assert!(matched);
+	}
+	#[test]
+	fn include_match() {
+		let includes = Some(Regex::new("Pinkie Pie").unwrap());
+		let matched = matches("Pinkie Pie is best pony!", &includes, &None);
+		assert!(matched);
+	}
+	#[test]
+	fn include_miss() {
+		let includes = Some(Regex::new("Rarity").unwrap());
+		let matched = matches("Pinkie Pie is best pony!", &includes, &None);
+		assert!(!matched);
+	}
+	#[test]
+	fn exclude_match() {
+		let excludes = Some(Regex::new("Pinkie").unwrap());
+		let matched = matches("Pinkie Pie is best pony!", &None, &excludes);
+		assert!(!matched);
+	}
+	#[test]
+	fn exclude_miss() {
+		let excludes = Some(Regex::new("Rarity").unwrap());
+		let matched = matches("Pinkie Pie is best pony!", &None, &excludes);
+		assert!(matched);
+	}
+	#[test]
+	fn include_match_exclude_miss() {
+		let includes = Some(Regex::new("Pinkie Pie").unwrap());
+		let excludes = Some(Regex::new("Rarity").unwrap());
+		let matched = matches("Pinkie Pie is best pony!", &includes, &excludes);
+		assert!(matched);
+	}
+	#[test]
+	fn include_miss_exclude_match() {
+		let includes = Some(Regex::new("Rarity").unwrap());
+		let excludes = Some(Regex::new("Pinkie Pie").unwrap());
+		let matched = matches("Pinkie Pie is best pony!", &includes, &excludes);
+		assert!(!matched);
+	}
+}
