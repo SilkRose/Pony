1fa1b3546cf65b1c359389ef8bfff1e09c1cede3
Silk Rose <silkrose@love-tolerance.com>
2024-08-15 00:26:56 -0400

    Use more macros in string number module in Pony
    Attempt to add more type support to string number, needs more work as two tests are failing.

--- code/src/string_number.rs
+++ code/src/string_number.rs
@@ -2,63 +2,101 @@ use serde::de::{Error, Visitor};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::{fmt, ops};
 
-impl fmt::Debug for StringNumber {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		fmt::Debug::fmt(&self.num, f)
-	}
+pub struct StringNumber<T> {
+	num: T,
 }
 
-impl fmt::Display for StringNumber {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		fmt::Display::fmt(&self.num, f)
-	}
+struct StringNumberVisitor<T> {
+	_marker: std::marker::PhantomData<T>,
 }
 
-impl ops::Deref for StringNumber {
-	type Target = f64;
-	fn deref(&self) -> &f64 {
-		&self.num
-	}
+macro_rules! string_number {
+	($($T:ty),+ $(,)?) => {
+		$(
+			impl fmt::Debug for StringNumber<$T> {
+				fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+					fmt::Debug::fmt(&self.num, f)
+				}
+			}
+
+			impl fmt::Display for StringNumber<$T> {
+				fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+					fmt::Display::fmt(&self.num, f)
+				}
+			}
+
+			impl ops::Deref for StringNumber<$T> {
+				type Target = $T;
+
+				fn deref(&self) -> &$T {
+					&self.num
+				}
+			}
+
+			impl ops::DerefMut for StringNumber<$T> {
+				fn deref_mut(&mut self) -> &mut $T {
+					&mut self.num
+				}
+			}
+
+			impl StringNumber<$T> {
+				pub fn to_number(&self) -> $T {
+					self.num
+				}
+			}
+
+			impl Serialize for StringNumber<$T> {
+				fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+				where
+					S: Serializer,
+				{
+					self.num.serialize(serializer)
+				}
+			}
+
+			impl<'de> Deserialize<'de> for StringNumber<$T> {
+				fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+				where
+					D: Deserializer<'de>,
+				{
+					deserializer.deserialize_any(StringNumberVisitor::<$T> {
+						_marker: std::marker::PhantomData,
+					})
+					.map(|num| Self { num })
+				}
+			}
+		)+
+	};
 }
 
-impl ops::DerefMut for StringNumber {
-	fn deref_mut(&mut self) -> &mut f64 {
-		&mut self.num
-	}
+macro_rules! string_number_integer {
+	($($T:ty),+ $(,)?) => {
+		$(impl<'de> Visitor<'de> for StringNumberVisitor<$T> {
+		type Value = $T;
+
+		fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+			formatter.write_str("a number, or a number in a string")
+		}
+		visit_method!(visit_i8 v: i8 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_i16 v: i16 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_i32 v: i32 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_i64 v: i64 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_i128 v: i128 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_u8 v: u8 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_u16 v: u16 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_u32 v: u32 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_u64 v: u64 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_u128 v: u128 { v.try_into().map_err(Error::custom) });
+		visit_method!(visit_str v: &str { v.parse().map_err(Error::custom) });
+		visit_method!(visit_bytes v: &[u8] {
+			std::str::from_utf8(v)
+				.map_err(Error::custom)
+				.and_then(|s| s.parse().map_err(Error::custom))
+			});
+		})+
+	};
 }
 
-impl StringNumber {
-	pub fn to_number(&self) -> f64 {
-		self.num
-	}
-}
-
-pub struct StringNumber {
-	num: f64,
-}
-
-impl Serialize for StringNumber {
-	fn serialize<S>(&self, ser: S) -> Result<S::Ok, S::Error>
-	where
-		S: Serializer,
-	{
-		ser.serialize_f64(self.num)
-	}
-}
-
-struct StringNumberVisitor;
-
-impl<'de> Deserialize<'de> for StringNumber {
-	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-	where
-		D: Deserializer<'de>,
-	{
-		let num = deserializer.deserialize_any(StringNumberVisitor)?;
-		Ok(Self { num })
-	}
-}
-
-/// lazy
 macro_rules! visit_method {
 	($method:ident $param:ident: $type:ty $body:block) => {
 		fn $method<E>(self, $param: $type) -> Result<Self::Value, E>
@@ -70,18 +108,52 @@ macro_rules! visit_method {
 	};
 }
 
-impl<'de> Visitor<'de> for StringNumberVisitor {
+impl<'de> Visitor<'de> for StringNumberVisitor<f32> {
+	type Value = f32;
+
+	fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+		formatter.write_str("a number, or a number in a string")
+	}
+	visit_method!(visit_f32 v: f32 { Ok(v) });
+	visit_method!(visit_i8 v: i8 { Ok(v.into()) });
+	visit_method!(visit_i16 v: i16 { Ok(v.into()) });
+	visit_method!(visit_u8 v: u8 { Ok(v.into()) });
+	visit_method!(visit_u16 v: u16 { Ok(v.into()) });
+	visit_method!(visit_str v: &str { v.parse().map_err(Error::custom) });
+	visit_method!(visit_bytes v: &[u8] {
+	std::str::from_utf8(v)
+		.map_err(Error::custom)
+		.and_then(|s| s.parse().map_err(Error::custom))
+	});
+}
+
+impl<'de> Visitor<'de> for StringNumberVisitor<f64> {
 	type Value = f64;
 
 	fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
 		formatter.write_str("a number, or a number in a string")
 	}
-
-	visit_method!(visit_i64 v: i64 { Ok(v as f64) });
-	visit_method!(visit_u64 v: u64 { Ok(v as f64) });
 	visit_method!(visit_f64 v: f64 { Ok(v) });
+	visit_method!(visit_i8 v: i8 { Ok(v.into()) });
+	visit_method!(visit_i16 v: i16 { Ok(v.into()) });
+	visit_method!(visit_i32 v: i32 { Ok(v.into()) });
+	visit_method!(visit_u8 v: u8 { Ok(v.into()) });
+	visit_method!(visit_u16 v: u16 { Ok(v.into()) });
+	visit_method!(visit_u32 v: u32 { Ok(v.into()) });
 	visit_method!(visit_str v: &str { v.parse().map_err(Error::custom) });
+	visit_method!(visit_bytes v: &[u8] {
+	std::str::from_utf8(v)
+		.map_err(Error::custom)
+		.and_then(|s| s.parse().map_err(Error::custom))
+	});
 }
+
+string_number!(f32, f64);
+string_number!(u8, u16, u32, u64, u128, usize);
+string_number!(i8, i16, i32, i64, i128, isize);
+
+string_number_integer!(u8, u16, u32, u64, u128, usize);
+string_number_integer!(i8, i16, i32, i64, i128, isize);
 
 #[cfg(test)]
 mod tests {
@@ -90,7 +162,7 @@ mod tests {
 
 	#[derive(Deserialize, Debug)]
 	struct Number {
-		number: StringNumber,
+		number: StringNumber<f64>,
 	}
 
 	#[test]
