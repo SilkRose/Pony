6c18f96a6becc7a0abd5cdb88797d3abbcb9dfe1
Silk Rose <silkrose@love-tolerance.com>
2024-08-18 13:58:32 -0400

    Finish text to image for string image module in Pony

--- code/src/string_image.rs
+++ code/src/string_image.rs
@@ -125,18 +125,17 @@ impl CharSet {
 		let lines = text.split('\n').collect::<Vec<_>>();
 		let height =
 			(self.line_height * lines.len() as u32) + (self.border.top + self.border.bottom);
-		let width = lines
+		let line_widths = lines
 			.iter()
 			.map(|line| {
 				line.chars()
 					.map(|c| self.chars.get(&c).unwrap().dimensions().0)
 					.sum::<u32>() + (line.len() as u32 * (self.spacing.letter - 1))
-					+ (self.border.left + self.border.right)
 			})
-			.max()
-			.unwrap();
+			.collect::<Vec<_>>();
+		let max_width = line_widths.iter().max().unwrap() + (self.border.left + self.border.right);
 
-		let mut image: RgbaImage = ImageBuffer::from_fn(width, height, |_, _| {
+		let mut image: RgbaImage = ImageBuffer::from_fn(max_width, height, |_, _| {
 			image::Rgba([
 				self.colors.background.rgba.red,
 				self.colors.background.rgba.green,
@@ -144,6 +143,40 @@ impl CharSet {
 				self.colors.background.rgba.alpha,
 			])
 		});
+
+		lines
+			.iter()
+			.zip(line_widths)
+			.enumerate()
+			.for_each(|(index, (line, width))| {
+				let index = index as u32;
+				let start_y =
+					self.border.top + (index * self.line_height) + (index * self.spacing.line);
+				let mut start_x = match self.justification {
+					Justification::Left => self.border.left,
+					Justification::CenterBreakLeft => {
+						((max_width - width) as f64 / 2.0).floor() as u32
+					}
+					Justification::CenterBreakRight => {
+						((max_width - width) as f64 / 2.0).ceil() as u32
+					}
+					Justification::Right => self.border.right - width,
+				};
+
+				for char in line.chars() {
+					let char_image = self.chars.get(&char).unwrap();
+					'pix: for pixel in char_image.pixels() {
+						let a = pixel.2 .0[3];
+						if a == 0 {
+							continue 'pix;
+						}
+						let (x, y) = (pixel.0 + start_x, pixel.1 + start_y);
+						let rgba = [pixel.2 .0[0], pixel.2 .0[1], pixel.2 .0[2], a];
+						image.put_pixel(x, y, image::Rgba(rgba));
+					}
+					start_x += char_image.dimensions().0 + self.spacing.letter;
+				}
+			});
 
 		if let Some(filepath) = filepath {
 			image.save(filepath)?;
