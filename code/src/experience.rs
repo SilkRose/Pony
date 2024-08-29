c366a47926b67fae7341841d93efcefeecdd7edb
Silk Rose <silkrose@love-tolerance.com>
2024-08-29 11:13:18 -0400

    Fix types in experience module in Pony

--- code/src/experience.rs
+++ code/src/experience.rs
@@ -1,18 +1,18 @@
 type Result<T, E = Box<dyn (::std::error::Error)>> = ::std::result::Result<T, E>;
 
 pub struct LevelSystem {
-	pub starting_level: i128,
-	pub current_level: i128,
-	pub xp_per_level: Vec<i128>,
-	pub xp_in_level: i128,
-	pub xp_to_level: i128,
-	pub algorithm: Box<dyn Fn(i128, i128) -> Result<i128>>,
+	pub starting_level: u128,
+	pub current_level: u128,
+	pub xp_per_level: Vec<u128>,
+	pub xp_in_level: u128,
+	pub xp_to_level: u128,
+	algorithm: Box<dyn Fn(u128, u128) -> Result<u128>>,
 }
 
 impl LevelSystem {
 	pub fn new(
-		starting_level: i128, first_level_xp: i128,
-		algorithm: Box<dyn Fn(i128, i128) -> Result<i128>>,
+		starting_level: u128, first_level_xp: u128,
+		algorithm: Box<dyn Fn(u128, u128) -> Result<u128>>,
 	) -> Self {
 		LevelSystem {
 			starting_level,
@@ -24,7 +24,7 @@ impl LevelSystem {
 		}
 	}
 
-	pub fn add_xp(mut self, xp: &mut i128) -> Result<Self> {
+	pub fn add_xp(mut self, xp: &mut u128) -> Result<Self> {
 		while xp >= &mut self.xp_to_level {
 			let next_level =
 				(self.algorithm)(self.current_level + 1, *self.xp_per_level.last().unwrap())?;
