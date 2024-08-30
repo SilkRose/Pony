b5c51ecdc942a2d307535c915a8986af8989091d
Silk Rose <silkrose@love-tolerance.com>
2024-08-30 03:48:56 -0400

    Refactor the experience module in Pony

--- code/src/experience.rs
+++ code/src/experience.rs
@@ -1,11 +1,9 @@
 type Result<T, E = Box<dyn (::std::error::Error)>> = ::std::result::Result<T, E>;
 
 pub struct LevelSystem {
-	pub starting_level: u128,
 	pub current_level: u128,
-	pub xp_per_level: Vec<u128>,
-	pub xp_in_level: u128,
-	pub xp_to_level: u128,
+	pub next_level_xp: u128,
+	pub xp_within_level: u128,
 	algorithm: Box<dyn Fn(u128, u128) -> Result<u128>>,
 }
 
@@ -15,20 +13,21 @@ impl LevelSystem {
 		algorithm: Box<dyn Fn(u128, u128) -> Result<u128>>,
 	) -> Self {
 		LevelSystem {
-			starting_level,
 			current_level: starting_level,
-			xp_per_level: vec![first_level_xp],
-			xp_in_level: 0,
-			xp_to_level: first_level_xp,
+			next_level_xp: first_level_xp,
+			xp_within_level: 0,
 			algorithm,
 		}
 	}
 
-	pub fn add_xp(mut self, xp: &mut u128) -> Result<Self> {
-		while xp >= &mut self.xp_to_level {
-			let next_level =
-				(self.algorithm)(self.current_level + 1, *self.xp_per_level.last().unwrap())?;
+	pub fn add_xp(mut self, xp: u128) -> Result<Self> {
+		let mut xp = xp;
+		while xp >= self.next_level_xp {
+			xp -= self.next_level_xp - self.xp_within_level;
+			self.xp_within_level = 0;
+			self.next_level_xp = (self.algorithm)(self.current_level, self.next_level_xp)?;
 		}
+		self.xp_within_level += xp;
 		Ok(self)
 	}
 }
