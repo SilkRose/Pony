4e19d1ef6506381ac90cefb4ddd86790eb5c4728
Silk Rose <silkrose@love-tolerance.com>
2024-10-08 05:28:30 -0400

    Update clean binary to format json files
    Also, cleans up the code some and adds logging.

--- code/src/bin/clean.rs
+++ code/src/bin/clean.rs
@@ -1,29 +1,45 @@
 use pony::fs::find_files_in_dir;
+use pony::json::{format_json, JsonFormat};
 use pony::regex::matches;
 use regex::Regex;
+use serde_json::Value;
 use std::error::Error;
 use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
+	let files = find_files_in_dir("../", true)?;
 	let includes = Some(Regex::new(r".*(stories|flash-fiction).*\.md$")?);
 	let excludes = Some(Regex::new(r".*archive.*|(-meta|readme)\.md$")?);
 	let single = Regex::new(r"[‘’´ʹ]")?;
 	let double = Regex::new(r"[“”‟″]")?;
-	find_files_in_dir("../", true)?
+	for file in files
 		.iter()
 		.filter(|file| matches(file, &includes, &excludes))
-		.for_each(|file| {
-			let mut data = fs::read_to_string(file).unwrap();
-			data = single.replace_all(&data, "'").to_string();
-			data = double.replace_all(&data, "\"").to_string();
-			data = data
-				.replace("...", "…")
-				.replace(",*", "*,")
-				.replace(",_", "_,")
-				.replace("---", "—")
-				.replace("--", "–");
+	{
+		println!("Formatting: {file}");
+		let mut data = fs::read_to_string(file)?;
+		data = single.replace_all(&data, "'").to_string();
+		data = double.replace_all(&data, "\"").to_string();
+		data = data
+			.replace("...", "…")
+			.replace(",*", "*,")
+			.replace(",_", "_,")
+			.replace("---", "—")
+			.replace("--", "–");
 
-			fs::write(file, data).unwrap();
-		});
+		fs::write(file, data)?;
+	}
+	let includes = Some(Regex::new(r".*\.json$")?);
+	let excludes = Some(Regex::new(
+		r".*(\.obsidian|\.vscode|code[/\\](dist|pony-temp|target)).*",
+	)?);
+	for file in files
+		.iter()
+		.filter(|file| matches(file, &includes, &excludes))
+	{
+		println!("Formatting: {file}");
+		let data: Value = serde_json::from_str(&fs::read_to_string(file)?)?;
+		fs::write(file, format_json(&data, JsonFormat::Tab)?.as_bytes())?;
+	}
 	Ok(())
 }
