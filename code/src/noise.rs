ab80edb16835934b1db72b2164473e3d7161ab96
Silk Rose <silkrose@love-tolerance.com>
2024-10-13 07:50:30 -0400

    Remove integer based noise from noise module

--- code/src/noise.rs
+++ code/src/noise.rs
@@ -12,53 +12,7 @@ fn lerp(t: f64, a: f64, b: f64) -> f64 {
 	a + t * (b - a)
 }
 
-pub struct PerlinNoise1DInt {
-	/// Seed value to generate noise from.
-	pub seed: u64,
-	/// Minimum value for any noise result.
-	pub minimum: f64,
-	/// Maximum value for any noise result.
-	pub maximum: f64,
-	/// Spacing between lattices.
-	pub frequency: i64,
-	/// How much each octave contributes.
-	pub amplitude: f64,
-	/// Frequency multiplier for each octave.
-	pub lacunarity: f64,
-	/// How much to adjust amplitude by.
-	pub persistence: f64,
-	/// Number of octaves to combine.
-	pub octaves: u32,
-}
-
-impl PerlinNoise1DInt {
-	pub fn get_point(&self, x: i64) -> Result<f64> {
-		let mut noise = 0.0;
-		let mut max_noise = 0.0;
-		let maximum = self.maximum - self.minimum;
-		let mut frequency = self.frequency;
-		let mut amplitude = self.amplitude;
-		for _ in 0..self.octaves {
-			let lower_lattice = x - (x % frequency);
-			let upper_lattice = lower_lattice + frequency;
-			let mut rng = StdRng::seed_from_u64(self.seed.wrapping_add_signed(lower_lattice));
-			let lower_noise = rng.gen_range(self.minimum..=self.maximum);
-			rng = StdRng::seed_from_u64(self.seed.wrapping_add_signed(upper_lattice));
-			let upper_noise = rng.gen_range(self.minimum..=self.maximum);
-			let interpolation_factor = (x - lower_lattice) as f64 / frequency as f64;
-			let interpolation_factor = fade(interpolation_factor);
-			let n = (1.0 - interpolation_factor) * lower_noise + interpolation_factor * upper_noise;
-			noise += n * amplitude;
-			max_noise += amplitude * maximum;
-			frequency *= self.lacunarity as i64;
-			amplitude *= self.persistence;
-		}
-		noise = maximum * (noise / max_noise);
-		Ok(noise)
-	}
-}
-
-pub struct PerlinNoise1DFloat {
+pub struct PerlinNoise1D {
 	/// Seed value to generate noise from.
 	pub seed: u64,
 	/// Amount to scale x by before calculation.
@@ -79,7 +33,7 @@ pub struct PerlinNoise1DFloat {
 	pub octaves: u8,
 }
 
-impl PerlinNoise1DFloat {
+impl PerlinNoise1D {
 	pub fn get_point(&self, x: f64) -> Result<f64> {
 		let mut x = x * self.scale;
 		let mut noise = 0.0;
@@ -117,44 +71,15 @@ mod test {
 	#[test]
 	fn one_dimension() -> Result<()> {
 		for octave in 1..=8 {
-			let noise = PerlinNoise1DInt {
+			let noise = PerlinNoise1D {
 				seed: 7669,
+				scale: 0.01,
 				minimum: 0.0,
 				maximum: 100.0,
-				frequency: 1,
-				amplitude: 1.0,
-				lacunarity: 4.0,
-				persistence: 2.0,
-				octaves: octave,
-			};
-			let mut values = vec![];
-			for x in 0..1000 {
-				values.push(noise.get_point(x)?.round() as u32);
-			}
-			let image: RgbaImage = ImageBuffer::from_fn(1000, 100, |x, y| {
-				if y > values[x as usize] {
-					Rgba([0, 0, 0, 255])
-				} else {
-					Rgba([255, 255, 255, 255])
-				}
-			});
-			image.save(format!("i-{octave}.png"))?;
-		}
-		Ok(())
-	}
-
-	#[test]
-	fn one_dimension_float() -> Result<()> {
-		for octave in 1..=8 {
-			let noise = PerlinNoise1DFloat {
-				seed: 7669,
-				scale: 0.001,
-				minimum: 0.0,
-				maximum: 100.0,
-				frequency: 4.0,
+				frequency: 1.2,
 				amplitude: 1.0,
 				lacunarity: 2.0,
-				persistence: 0.25,
+				persistence: 0.5,
 				octaves: octave,
 			};
 			let mut values = vec![];
@@ -168,7 +93,7 @@ mod test {
 					Rgba([255, 255, 255, 255])
 				}
 			});
-			image.save(format!("f-{octave}.png"))?;
+			image.save(format!("{octave}.png"))?;
 		}
 		Ok(())
 	}
