a04cf98de66769e491b37d055dbcde01f02f2d73
Silk Rose <silkrose@love-tolerance.com>
2025-03-03 23:28:55 -0500

    Rewrite types and remove old code for April Fools code in Pony

--- code/src/bin/april-fools.rs
+++ code/src/bin/april-fools.rs
@@ -1,25 +1,90 @@
-use async_recursion::async_recursion;
 use pony::command::execute_command;
-use reqwest::Client;
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE, HeaderMap, HeaderValue};
+use reqwest::{Client, Response};
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
 use std::collections::HashMap;
 use std::env;
+use std::error::Error;
 use std::fs;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::time::timeout;
 use wiwi::prelude::*;
 
+type Events = HashMap<String, Event>;
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
-struct Event {
-	release_hour: u32,
-	release_minute: u32,
-	title: Option<String>,
+enum Event {
+	Chapter(Chapter),
+	VoteResult(VoteResult),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Chapter {
+	// ID of the next vote result event.
+	next_event: Option<String>,
+	// ID of the next chapter event.
+	vote_result_event: Option<String>,
+	// Duration of the event.
+	duration: u32,
+	// Cover file name, no path included.
 	cover: Option<String>,
-	chapter_id: Option<u32>,
-	description: Option<String>,
-	short_description: Option<String>,
-	completion_status: Option<String>,
+	// Title for setting when no vote is taking place.
+	like_delta: i32,
+	// Content to post.
+	content: Option<String>,
+	// The title for before the vote passes.
+	title_below: String,
+	// The title for when exact votes are met.
+	title_exact: Option<String>,
+	// The title for after the vote passes.
+	title_above: Option<String>,
+	// Long description.
+	description: String,
+	// The short description for before the vote passes.
+	short_description_below: String,
+	// The short description for when exact votes are met.
+	short_description_exact: Option<String>,
+	// The short description for after the vote passes.
+	short_description_above: Option<String>,
+	// Authors note.
+	authors_note: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct VoteResult {
+	// The title for if the vote fails.
+	title_fail: String,
+	// The title for if the vote passes.
+	title_pass: String,
+	// Short description for if the vote fails.
+	short_description_fail: String,
+	// Short description for if the vote passes.
+	short_description_pass: String,
+	// Content to pre-pend if the vote passes.
+	content_pass: String,
+	// Content to pre-pend if the vote fails.
+	content_fail: String,
+	// Replacements for if the vote passes.
+	content_pass_replace: Option<HashMap<String, String>>,
+	// Replacements for if the vote fails.
+	content_fail_replace: Option<HashMap<String, String>>,
+	// Authors note.
+	authors_note: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+struct EventState {
+	// Likes at the start of the last vote.
+	likes: u32,
+	// Minutes into the current event.
+	elapsed: u32,
+	// Last event.
+	chapter: Option<String>,
+	// Replacements for passed votes.
+	content_pass_replace: HashMap<String, String>,
+	// Replacements for failed votes.
+	content_fail_replace: HashMap<String, String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -31,6 +96,7 @@ struct Arguments {
 	interval_minutes: i64,
 	countdown_duration_hours: i64,
 	covers_dir: String,
+	content_dir: String,
 	cover_mane_js: String,
 	fimfic_cookie_json: String,
 }
@@ -39,6 +105,16 @@ struct Arguments {
 struct Urls {
 	story: String,
 	chapter: String,
+}
+
+#[derive(Debug, Clone)]
+struct FimficRequest {
+	client: Client,
+	headers: HeaderMap,
+	interval: Duration,
+	interval_step: Duration,
+	interval_max: Duration,
+	timeout: Duration,
 }
 
 #[tokio::main]
@@ -61,14 +137,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		chapter: "https://www.fimfiction.net/api/v2/chapters".to_string(),
 	};
 
-	let client = Client::new();
-
-	let mut headers = HeaderMap::new();
-	headers.insert(
-		AUTHORIZATION,
-		HeaderValue::from_str(&format!("Bearer {}", api_token))?,
-	);
-	headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+	// API and site request structs, client, headers, and time intervals.
+	let api = FimficRequest {
+		client: Client::new(),
+		headers: setup_api_headers(&api_token)?,
+		interval: Duration::from_millis(500),
+		interval_step: Duration::from_secs(2),
+		interval_max: Duration::from_secs(120),
+		timeout: Duration::from_secs(10),
+	};
 
 	let mut timer = ClockTimer::builder()
 		.with_start_datetime(DateTime::from_timestamp(args.start_time, 0).unwrap())
@@ -76,114 +153,85 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		.with_interval(TimeDelta::try_minutes(args.interval_minutes).unwrap())
 		.build();
 
+	let mut current_state = EventState::default();
 	while let Some(tick) = timer.tick().await {
 		if args.skip_past_events && tick.past_due() {
 			continue;
 		}
-		let countdown = tick.remaining().num_minutes()
-			>= (args.duration_hours - args.countdown_duration_hours) * 60;
-		handle_events(
-			args.clone(),
-			events.clone(),
-			countdown,
-			urls.clone(),
-			client.clone(),
-			headers.clone(),
-			tick,
+
+		let elapsed = tick.elapsed();
+		let remaining = tick.remaining();
+	}
+
+	Ok(())
+}
+
+fn setup_api_headers(token: &str) -> Result<HeaderMap, Box<dyn Error>> {
+	let mut headers = HeaderMap::new();
+	headers.insert(
+		AUTHORIZATION,
+		HeaderValue::from_str(&format!("Bearer {}", token))?,
+	);
+	headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+	Ok(headers)
+}
+
+macro_rules! api_request {
+	($fun:ident, $method:ident) => {};
+}
+
+api_request!(api_post_request, post);
+api_request!(api_patch_request, patch);
+
+async fn api_get_request(
+	request: &FimficRequest, url: &str,
+) -> Result<Response, Box<dyn std::error::Error + Send + Sync>> {
+	let mut interval = request.interval;
+	loop {
+		let start_time = unix_time()?;
+		let res = timeout(
+			request.timeout,
+			request
+				.client
+				.get(url)
+				.headers(request.headers.clone())
+				.send(),
 		)
 		.await;
-	}
-
-	Ok(())
-}
-
-async fn handle_events(
-	args: Arguments, events: Vec<Event>, countdown: bool, urls: Urls, client: Client,
-	headers: HeaderMap, tick: Tick,
-) {
-	let elapsed = tick.elapsed();
-	let remaining = tick.remaining();
-	let remaining_hours =
-		(remaining.num_minutes() / 60) - (args.duration_hours - args.countdown_duration_hours);
-	let remaining_minutes = match remaining.num_minutes() % 60 == 0 {
-		true => 0,
-		false => {
-			remaining.num_minutes()
-				- ((args.duration_hours - args.countdown_duration_hours) * 60)
-				- (remaining_hours * 60)
-		}
-	};
-	let title = match countdown {
-		true => Some(format!(
-			"This Story will Explode in {:0>2}:{:0>2}",
-			remaining_hours, remaining_minutes
-		)),
-		false => None,
-	};
-	let events = events
-		.iter()
-		.filter(|event| {
-			event.release_hour == elapsed.num_hours() as u32
-				&& event.release_minute
-					== (elapsed.num_minutes() - (elapsed.num_hours() * 60)) as u32
-		})
-		.collect::<Vec<_>>();
-	if !events.is_empty() {
-		for event in events {
-			let story_json = story_json(
-				args.story_id,
-				&title.clone().or(event.title.clone()),
-				&event.short_description,
-				&event.description,
-				&event.completion_status,
-			);
-			println!("{}", serde_json::to_string_pretty(&story_json).unwrap());
-			let _ = send_api_request(&client, &headers, &urls.story, story_json, 0).await;
-			if event.chapter_id.is_some() {
-				let chapters_url = format!("{}/{}", urls.chapter, event.chapter_id.unwrap());
-				let chapter_json = chapter_json(event.chapter_id.unwrap()).to_string();
-				let _ = send_api_request(&client, &headers, &chapters_url, chapter_json, 0).await;
+		match res {
+			Ok(Ok(response)) => {
+				return Ok(response);
 			}
-			if event.cover.is_some() {
-				let cover = format!("{}{}", args.covers_dir, event.cover.as_ref().unwrap());
-				let command = format!(
-					r#"node "{}" {} "{}" "{}""#,
-					args.cover_mane_js, args.story_id, cover, args.fimfic_cookie_json
-				);
-
-				execute_command(&command).unwrap();
+			Ok(Err(error)) => {
+				println!("Request failed: {error}");
+			}
+			Err(error) => {
+				println!("Request timed out: {error}");
 			}
 		}
-	} else {
-		if title.is_none() {
-			return;
-		}
-		let story_json = story_json(args.story_id, &title, &None, &None, &None);
-		println!("{}", serde_json::to_string_pretty(&story_json).unwrap());
-		let _ = send_api_request(&client, &headers, &urls.story, story_json, 0).await;
+		sleep(start_time, interval).await?;
+		interval = if interval < request.interval_max {
+			interval + request.interval_step
+		} else {
+			request.interval_max
+		};
 	}
 }
 
-#[async_recursion]
-async fn send_api_request(
-	client: &Client, headers: &HeaderMap, url: &str, body: String, recursion_level: u32,
-) -> Result<(), Box<dyn std::error::Error>> {
-	let api_response = client
-		.patch(url)
-		.headers(headers.clone())
-		.body(body.clone())
-		.send()
-		.await?;
-	if !api_response.status().is_success() {
-		println!("{:?}", api_response.text().await);
-		if recursion_level > 4 {
-			eprintln!("Failed to send API request five times!\n{url}\n{body}");
-			return Ok(());
-		}
-		tokio::time::sleep(Duration::from_millis(5000)).await;
-		return send_api_request(client, headers, url, body, recursion_level + 1).await;
-	}
+async fn sleep(
+	start_time: u128, interval: Duration,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+	let current_time = unix_time()?;
+	let elapsed_time = Duration::from_millis((current_time - start_time).try_into()?);
+	if elapsed_time > interval {
+		return Ok(());
+	};
+	tokio::time::sleep(interval - elapsed_time).await;
 	Ok(())
+}
+
+fn unix_time() -> Result<u128, Box<dyn std::error::Error + Send + Sync>> {
+	Ok(SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis())
 }
 
 fn chapter_json(id: u32) -> Value {
