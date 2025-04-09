6230540853e946051da383fb12e1b1468b2b0d7f
Silk Rose <silkrose@love-tolerance.com>
2025-04-08 21:43:10 -0400

    Add chapter structs for Fimfiction API module in Pony

---
+++ code/src/fimfiction_api/chapter.rs
@@ -0,0 +1,44 @@
+use super::{ApiDebug, ApiLinks, ApiMeta, RelationshipData, story::StoryData, user::UserData};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ChapterApi<T = u32> {
+	pub data: ChapterData<T>,
+	pub included: Vec<ChapterIncluded>,
+	pub uri: String,
+	pub method: String,
+	pub debug: ApiDebug,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ChapterIncluded {
+	Story(StoryData),
+	Author(UserData),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ChapterData<T = u32> {
+	pub id: String,
+	pub r#type: String,
+	pub attributes: ChapterAttributes<T>,
+	pub relationships: ChapterRelationship,
+	pub links: ApiLinks,
+	pub meta: ApiMeta,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ChapterRelationship {
+	story: RelationshipData,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ChapterAttributes<T = u32> {
+	pub chapter_number: T,
+	pub title: String,
+	pub published: bool,
+	pub num_words: T,
+	pub num_views: T,
+	pub date_published: String,
+	pub date_modified: String,
+	pub authors_note_position: String,
+}
