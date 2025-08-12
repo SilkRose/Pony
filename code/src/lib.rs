92027521f281053964cea21b2ac8d8a0be3f2696
Silk Rose <silkrose@love-tolerance.com>
2025-08-11 22:44:14 -0400

    Remove malloc module from Pony

--- code/src/lib.rs
+++ code/src/lib.rs
@@ -23,34 +23,3 @@ pub mod threads;
 pub mod time;
 pub mod traits;
 pub mod word_stats;
-
-#[allow(clippy::tabs_in_doc_comments)]
-/// Fix for glibc memory bug.
-///
-/// glibc malloc has a long standing bug
-/// causing memory hoarding.
-///
-/// This module fixes that.
-/// Code originally written by [Michael Murphy](https://github.com/mmstick).
-///
-/// Re-licensed under MIT with permission.
-///
-/// To use it, simply add the following to your mane function:
-/// ```rs
-/// 	pony::malloc::limit_mmap_threshold(65_536);
-/// ```
-///
-/// It is also suggested to call `trim(0)`
-/// from time to time or in a loop.
-/// ```rs
-/// 	pony::malloc::trim(0);
-/// ```
-///
-/// More details on this can be read from the following:
-/// - <https://github.com/pop-os/libcosmic/blob/master/src/malloc.rs>
-/// - <https://fosstodon.org/@mmstick/113952008189644564>
-/// - <https://github.com/pop-os/cosmic-bg/pull/73>
-/// - <https://www.man7.org/linux/man-pages/man3/mallopt.3.html>
-/// - <https://www.man7.org/linux/man-pages/man3/malloc_trim.3.html>
-#[cfg(target_env = "gnu")]
-pub mod malloc;
