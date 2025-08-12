ce7b4a0335726b6276499295c9e9f2c6801420d7
Silk Rose <silkrose@love-tolerance.com>
2025-08-12 05:18:13 -0400

    Update hash module in Pony
    Makes chunk size configurable with a default of 64KB. Adds a blake3 hash function. Done with help from @meadowsys.

--- code/src/hash.rs
+++ code/src/hash.rs
@@ -1,18 +1,53 @@
 use sha2::{Digest, Sha256, Sha512};
-use std::{fs, io};
+use std::fs;
+use std::io::Read;
+use std::num::NonZeroUsize;
 
-type Result<T, E = Box<dyn (::std::error::Error)>> = ::std::result::Result<T, E>;
+type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
-pub fn get_hash_sha256(filename: &str) -> Result<String> {
-	let mut file = fs::File::open(filename)?;
+const DEFAULT_CHUNK_SIZE: NonZeroUsize = NonZeroUsize::new(65536).unwrap();
+
+pub fn get_hash_sha256(filename: &str, chunk_size: Option<NonZeroUsize>) -> Result<String> {
+	let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE).into();
+	let mut file = fs::OpenOptions::new().read(true).open(filename)?;
 	let mut hasher = Sha256::new();
-	io::copy(&mut file, &mut hasher)?;
+	let mut buf = vec![0u8; chunk_size];
+	loop {
+		let n = file.read(&mut buf)?;
+		if n == 0 {
+			break;
+		}
+		hasher.update(&buf[..n]);
+	}
 	Ok(format!("{:x}", hasher.finalize()))
 }
 
-pub fn get_hash_sha512(filename: &str) -> Result<String> {
-	let mut file = fs::File::open(filename)?;
+pub fn get_hash_sha512(filename: &str, chunk_size: Option<NonZeroUsize>) -> Result<String> {
+	let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE).into();
+	let mut file = fs::OpenOptions::new().read(true).open(filename)?;
 	let mut hasher = Sha512::new();
-	io::copy(&mut file, &mut hasher)?;
+	let mut buf = vec![0u8; chunk_size];
+	loop {
+		let n = file.read(&mut buf)?;
+		if n == 0 {
+			break;
+		}
+		hasher.update(&buf[..n]);
+	}
 	Ok(format!("{:x}", hasher.finalize()))
+}
+
+pub fn get_hash_blake3(filename: &str, chunk_size: Option<NonZeroUsize>) -> Result<String> {
+	let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE).into();
+	let mut file = fs::OpenOptions::new().read(true).open(filename)?;
+	let mut hasher = blake3::Hasher::new();
+	let mut buf = vec![0u8; chunk_size];
+	loop {
+		let n = file.read(&mut buf)?;
+		if n == 0 {
+			break;
+		}
+		hasher.update(&buf[..n]);
+	}
+	Ok(hasher.finalize().to_hex().to_string())
 }
